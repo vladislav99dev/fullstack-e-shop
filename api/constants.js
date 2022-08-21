1fb81b8cd4a74953e05592f776e4b8aaf15fcefd
vladislav99dev <vladislavcho99@abv.bg>
2022-08-21 13:48:41 +0300

    add constants connection string and port

---
+++ api/constants.js
@@ -0,0 +1,9 @@
+const PORT = 3030;
+
+const dbConnectionString = `mongodb+srv://vladi8199:f86s7PEZxAQBVWYY@e-shop.i2wtnno.mongodb.net/?retryWrites=true&w=majority`;
+
+
+module.exports = {
+    PORT,
+    dbConnectionString
+}
