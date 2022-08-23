9bbb458e98355e49852366c848d27d8e66f35c3d
vladislav99dev <vladislavcho99@abv.bg>
2022-08-23 15:58:50 +0300

    add logout handler

--- api/controllers/userControllers.js
+++ api/controllers/userControllers.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const userServices = require("../services/userServices");
 
@@ -14,6 +15,7 @@ const registerHandler = async(req,res)=> {
           const user = await userServices.findOne(email);
           const isUserFound = Boolean(user);
           if(isUserFound) return res.status(409).json({error:'User with this email already exist!'});
+          
           const dbResponse = await userServices.create(
               firstName,
               lastName,
@@ -39,19 +41,48 @@ const loginHandler = async(req,res) => {
   if(!email || !password) return res.status(400).json({error:'Email and Password are required in order to continue'});
     try {
         const user = await userServices.findOne(email);
+
         const isUserFound = Boolean(user);
         if(!isUserFound) return res.status(404).json({error:'Incorrect email or password'});
+        
         const isValid = await bcrypt.compare(password,user.password);
         if(!isValid) return res.status(404).json({error:'Incorrect email or password'});
-        //send jwt and user info
-        return res.json({congrats:'You successfully logged in'});
+        
+        delete user.password
+        const accessToken = generateAccessToken(user);
+        return res.status(200).json({user,accessToken});
     } catch(err){
         console.log(err);
     }
 }
 
+const logoutHandler = async(req,res) => {
+  console.log(`POST ${req.originalUrl}`);
+
+  const token = req.headers.authorization;
+  if(!token) return res.status(401).json({error:'No access token provided!'});
+   
+  try {
+    const decodedToken = verifyAccessToken(token);
+    res.status(200).json({successMessage: 'You have successfully loged out!'});
+  } catch(err) {
+    return res.status(401).json({error:'Your accessToken have expired!'});
+  }
+}
+
+
+const verifyAccessToken = (token) => {
+   return jwt.verify(token,process.env.ACCESS_TOKEN_SECRET);
+}
+
+const generateAccessToken = (user) => {
+  return jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn: '30s'});
+}
+
 router.post("/register", registerHandler);
 router.post("/login", loginHandler);
+router.post("/logout", logoutHandler);
+
 
 
 module.exports = router;
