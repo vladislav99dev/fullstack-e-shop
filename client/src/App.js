9f3954dfee4f05d7aa0d16b693a68791d9dc59c2
vladislav99dev <vladislavcho99@abv.bg>
2022-08-28 19:49:07 +0300

    use autContext

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import { Routes,Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import NavBar from './components/NavBar/NavBar.js'
-import Login from './components/Login/Login.js';
+import NavBar from "./components/NavBar/NavBar.js";
+import Login from "./components/Login/Login.js";
+
+import { AuthProvider } from "./context/AuthContext.js";
 
 function App() {
-  return(
-    <div id='container'>
-      <NavBar /> 
-      <div id='main-content' className='mt-10 h-full w-full'>
-      <Routes>
-        <Route path='/users/'>
-            <Route path='login' element={<Login />}/>
-            {/* <Route path='register' element={<Login />}/>
+  return (
+    <AuthProvider>
+      <div id="container">
+        <NavBar />
+        <div id="main-content" className="mt-10 h-full w-full">
+          <Routes>
+            <Route path="/users/">
+              <Route path="login" element={<Login />} />
+              {/* <Route path='register' element={<Login />}/>
             <Route path='logout' element={<Login />}/> */}
-        </Route>
-      </Routes>
+            </Route>
+          </Routes>
+        </div>
       </div>
-    </div>
-  )
+    </AuthProvider>
+  );
 }
 
 export default App;
