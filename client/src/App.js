8620c7cd1368c3414611e5a17d44fee094790cf4
vladislav99dev <vladislavcho99@abv.bg>
2022-08-30 21:18:26 +0300

    Add logout

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { AuthProvider } from "./context/AuthContext.js";
 import NavBar from "./components/NavBar/NavBar.js";
 import Login from "./components/Login/Login.js";
 import Register from "./components/Register/Register.js";
+import Logout from "./components/Logout.js/Logout.js";
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
             <Route path="/users/">
               <Route path="login" element={<Login />} />
               <Route path='register' element={<Register />}/>
+              <Route path='logout' element={<Logout />}/>
+
             {/* <Route path='logout' element={<Login />}/> */}
             </Route>
           </Routes>
--- client/src/services/userRequester.js
+++ client/src/services/userRequester.js
@@ -3,20 +3,47 @@ const requester = (service,data) => {
     let url ='';
     let options = {};
     if(service === 'login'){
-    url = `http://localhost:3030/users/login`
-    Object.assign(options,{
-        method:"POST",
-        headers:{
-            'content-type':'application/json'
-        },
-        body:JSON.stringify({
-            email: data.email,
-            password: data.password
+        url = `http://localhost:3030/users/login`
+        Object.assign(options,{
+            method:"POST",
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify({
+                email: data.email,
+                password: data.password
+            })
         })
-    })
+    }
+    if(service === 'register'){
+        url = `http://localhost:3030/users/register`
+        Object.assign(options,{
+            method:"POST",
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify({
+                firstName:data.firstName,
+                lastName:data.lastName,
+                email: data.email,
+                password: data.password,
+                country:data.country,
+                city:data.city,
+                street:data.street
+            })
+        })
+    }
+    if(service === 'logout'){
+        url = `http://localhost:3030/users/logout`
+        Object.assign(options, {
+            method: "POST"
+        })
     }
     return fetch(url,options)
 }
 
 
 export const login = requester.bind(null,'login')
+export const register = requester.bind(null,'register')
+export const logout = requester.bind(null,'logout')
+
