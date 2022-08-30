91d58b197687cfef7771febdc5f6700295234059
vladislav99dev <vladislavcho99@abv.bg>
2022-08-30 21:19:06 +0300

    register form validations

--- client/src/services/formValidationsServices.js
+++ client/src/services/formValidationsServices.js
@@ -2,7 +2,7 @@ const validate = (service, data) => {
   let messages = [];
   if (service === "login") {
     if (
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email) === false
+      !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email)
     ) {
       messages.push("You should enter valid email!");
     }
@@ -11,7 +11,40 @@ const validate = (service, data) => {
         messages.push("Password should be at least 6 characters long!")
     }
   }
-  return messages;
+
+  if(service === 'register') {
+    if(!/^[a-z-']+$/i.test(data.firstName)){
+      messages.push("Fisrt name is not in a valid format!")
+    }
+    if(!/^[a-z-']+$/i.test(data.lastName)){
+      messages.push("Last name is not in valid format!")
+    }
+    if (
+      !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email)
+    ) {
+      messages.push("Email is not in valid format!");
+    }
+    if(data["password"] !== data["re-password"]){
+      messages.push("Passwords are not same!")
+    }
+    if(!/[0-9]/.test(data.password)){
+      messages.push("Password should be only numbers!")
+    }
+    if(data.password.length < 6){
+      messages.push("Password should be at least 6 characters long!")
+    }
+    if(!/^[a-z -']+$/i.test(data.country)){
+      messages.push("Country is not in valid format!")
+    }
+    if(!/^[a-z -']+$/i.test(data.city)){
+      messages.push("City is not in valid format!")
+    }
+    if(!/^[a-z -'0-9]+$/i.test(data.street)){
+      messages.push("Street address is not in valid format!")
+    }
+
+  }
+  return messages
 };
 
 export const validateRegister = validate.bind(null, "register");
