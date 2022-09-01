1f05b4c47555b05112847f718598e83c7bde5ca5
vladislav99dev <vladislavcho99@abv.bg>
2022-09-01 16:54:12 +0300

    abstract userLinks

--- client/src/components/NavBar/UserLinks.js
+++ client/src/components/NavBar/UserLinks.js
@@ -2,28 +2,29 @@ import { Link } from "react-router-dom";
 
 
 import { useAuthContext } from "../../context/AuthContext"
-import { useNavTogglesContext } from "../../context/NavTogglesContext";
 
-const UserLinks = () => {
+const UserLinks = ({
+    clickHandler
+}) => {
     const {user} = useAuthContext();
-    const {toggleDesktopUserMenu} = useNavTogglesContext();
+
     return(
         <>
         {!user.email
         ?<>
             <div className="container-links">
-                <Link to={"users/login"} className="mobile-links" onClick={toggleDesktopUserMenu} >Login</Link>
+                <Link to={"users/login"} className="mobile-links" onClick={clickHandler} >Login</Link>
             </div>
             <div className="container-links">
-                <Link to={"users/register"} className="mobile-links" onClick={toggleDesktopUserMenu}>Register</Link>
+                <Link to={"users/register"} className="mobile-links" onClick={clickHandler}>Register</Link>
             </div>
         </>
         :<>
             <div className="container-links">
-                <Link to={"users/logout"} className="mobile-links" onClick={toggleDesktopUserMenu}>Logout</Link>
+                <Link to={"users/logout"} className="mobile-links" onClick={clickHandler}>Logout</Link>
             </div>
             <div className="container-links">
-                <Link to={"#"} className="mobile-links" onClick={toggleDesktopUserMenu}>Edit Profile</Link>
+                <Link to={"#"} className="mobile-links" onClick={clickHandler}>Edit Profile</Link>
             </div>
         </>
         }
