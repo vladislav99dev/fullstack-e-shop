d90fb07ac18ff588cdb443105e4f9ddee87ffb6b
vladislav99dev <vladislavcho99@abv.bg>
2022-09-01 13:44:47 +0300

    Abtract Modal

--- client/src/components/Modals/Modal.js
+++ client/src/components/Modals/Modal.js
@@ -6,10 +6,10 @@ const Modal = ({
     buttonHandler,
     buttonName
 }) => {
-    // const navigate = useNavigate();
-    // const cancelHandler = () => {
-    //     navigate(-1)
-    // }
+    const navigate = useNavigate();
+    const cancelHandler = () => {
+        navigate(-1)
+    }
     return (
     <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
 
@@ -35,13 +35,12 @@ const Modal = ({
                  </div> 
                 : null
             }
-
                 </div>
             </div>
             </div>
             <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-            <button type="button" className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm" >{buttonName}</button>
-            <button type="button" className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>
+            <button type="button" className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm" onClick={buttonHandler}>{buttonName}</button>
+            <button type="button" className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" onClick={cancelHandler}>Cancel</button>
             </div>
         </div>
         </div>
