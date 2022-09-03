019bcde895d990e66986e16510e348d0f3de99d3
vladislav99dev <vladislavcho99@abv.bg>
2022-09-03 21:23:52 +0300

    Product schema

---
+++ api/models/Products/Product.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const productsSchema = new mongoose.Schema({
+    type:{
+        required:[true,'Type is required'],
+        type: String
+    },
+    category:{
+        required:[true,'Type is required'],
+        type:String,
+    },
+    gender:{
+        required:[true, 'Gender is required'],
+        type:String, 
+    },
+    brand:{
+        required:[true,'Shoe brand is required'],
+        type:String,
+    },
+    imageUrl:{
+        required:[true,'Image  is required'],
+        type:String,
+    },
+    color:{
+        required:[true,'Shoe color is required'],
+        type:String,
+    },
+    price:{
+        required:[true,'Shoe price is required'],
+        type:Number,
+    },
+    sizes:{
+        // for some fuking reason mongoose lib throws error
+    },
+    onSale:{
+        type:Boolean,
+        default: false
+    }
+})
+
+
+const Product = mongoose.model("Product", productsSchema);
+
+module.exports = Product;
