34529de1b0813270bc76afde19d16968352df188
vladislav99dev <vladislavcho99@abv.bg>
2022-09-03 21:24:23 +0300

    add functionallity if token is not presented

--- api/middlewares/isAdmin.js
+++ api/middlewares/isAdmin.js
@@ -3,15 +3,15 @@ const tokenServices = require('../services/tokenServices')
 
 const isAdmin = (req, res, next) => {
     const token = req.headers.authorization;
+    if(token === 'undefined') return res.status(401).json({isAdmin:false, message: 'Token is not provided!'})
     try{
         const admin = tokenServices.verify(token);
-        req.isAdmin = {isAdmin:true, message:'Token is valid.', admin};
     } catch(err){
         const[error,errorMessage] = Object.values(err)
         if(errorMessage === 'invalid token') return res.status(401).json({isAdmin:false,message:'Token is invalid!'});
         if(errorMessage === 'jwt expired') return  res.status(401).json({isAdmin:false,message:'Token expired!'});
     }
-    next();
+    next()
 }
 
 module.exports = isAdmin
