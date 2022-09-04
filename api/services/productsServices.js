76e977e58e26d98441f69d0ef2414a06bdae03f8
vladislav99dev <vladislavcho99@abv.bg>
2022-09-04 20:47:04 +0300

    add get one and findbyidand update services

--- api/services/productsServices.js
+++ api/services/productsServices.js
@@ -4,8 +4,16 @@ const Product = require('../models/Products/Product')
  const create = (type,category,gender,brand,imageUrl,color,price,sizes) => {
     return Product.create({type,category,gender,brand,imageUrl,color,price,sizes})
 }
+const getOne = (id) => {
+    return Product.findById(id)
+} 
+const findOneAndUpdate = (data,id) => {
+    return Product.findByIdAndUpdate(id,data)
+}
 
 const productsServices = {
     create,
+    getOne,
+    findOneAndUpdate
 }
 module.exports = productsServices;
