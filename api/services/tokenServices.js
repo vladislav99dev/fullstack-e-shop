e7ed51922597441ef00f70789bc8a53903c7c09d
vladislav99dev <vladislavcho99@abv.bg>
2022-09-04 14:57:45 +0300

    make token last 1 day

--- api/services/tokenServices.js
+++ api/services/tokenServices.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
  }
  
  const generate = (user) => {
-   return jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1h'});
+   return jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1d'});
  }
 
  const tokenServices = {
