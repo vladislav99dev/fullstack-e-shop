c9de6a486532ed9bd364161c20eca79ce58df1b5
vladislav99dev <vladislavcho99@abv.bg>
2022-09-04 14:58:09 +0300

    add more descriptive hson messages from server responses

--- api/middlewares/isAdmin.js
+++ api/middlewares/isAdmin.js
@@ -3,13 +3,13 @@ const tokenServices = require('../services/tokenServices')
 
 const isAdmin = (req, res, next) => {
     const token = req.headers.authorization;
-    if(token === 'undefined') return res.status(401).json({isAdmin:false, message: 'Token is not provided!'})
+    if(token === 'undefined') return res.status(401).json({isAdmin:false, message: 'Access token is not provided!'})
     try{
         const admin = tokenServices.verify(token);
     } catch(err){
         const[error,errorMessage] = Object.values(err)
-        if(errorMessage === 'invalid token') return res.status(401).json({isAdmin:false,message:'Token is invalid!'});
-        if(errorMessage === 'jwt expired') return  res.status(401).json({isAdmin:false,message:'Token expired!'});
+        if(errorMessage === 'invalid token') return res.status(401).json({isAdmin:false,message:'Access token is invalid!'});
+        if(errorMessage === 'jwt expired') return  res.status(401).json({isAdmin:false,message:'Access token expired,you should re-login!'});
     }
     next()
 }
