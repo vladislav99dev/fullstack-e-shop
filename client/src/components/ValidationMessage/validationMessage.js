ac42ca580fc95f9153ac9dcbe31b9332a5d48006
vladislav99dev <vladislavcho99@abv.bg>
2022-09-07 16:05:32 +0300

    capitalize validation message

--- client/src/components/ValidationMessage/validationMessage.js
+++ client/src/components/ValidationMessage/validationMessage.js
@@ -2,7 +2,7 @@ const ValidationMessage = ({
     message
 }) => {
     return (
-        <p className="text-center italic text-[#ae0001] font-bold mt-2">{message}</p>
+        <p className="text-center italic text-[#ae0001] font-bold mt-2 capitalize">{message}</p>
     )
 }
 export default ValidationMessage
