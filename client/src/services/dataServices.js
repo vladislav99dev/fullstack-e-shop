0a689b8d47ec73cab7bfb31bfb18206e8d07cb13
vladislav99dev <vladislavcho99@abv.bg>
2022-09-07 16:05:45 +0300

    format code in dataServices

--- client/src/services/dataServices.js
+++ client/src/services/dataServices.js
@@ -1,4 +1,6 @@
-export const dataFormater = (data) => {
+
+
+export const dataSizeFormater = (data) => {
     data.sizes = data.sizes.split(',');
     data.sizes = data.sizes.map(x => x.split(':')) ;
     data.sizes = Object.fromEntries(data.sizes);
@@ -6,6 +8,7 @@ export const dataFormater = (data) => {
     for (const key in data.sizes) {
        data.sizes[key] = Number(data.sizes[key]);
     };
+
     return data;
 };
 
@@ -14,9 +17,3 @@ export const formDataExtracter = (form) => {
     const data = Object.fromEntries(formData);
     return data
 };
-
-export const extractAndFormatData = (form) => {
-    const data = formDataExtracter(form);
-    const formatedData = dataFormater(data);
-    return formatedData;
-}
