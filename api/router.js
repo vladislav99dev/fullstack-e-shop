e5385521406b1d4a2e415f2e86564c9d1ac9d665
vladislav99dev <vladislavcho99@abv.bg>
2022-09-09 21:03:33 +0300

    products controller

--- api/router.js
+++ api/router.js
@@ -3,9 +3,12 @@ const router = require('express').Router();
 const isAdmin = require('./middlewares/isAdmin')
 const userController = require('./controllers/userControllers')
 const adminController = require('./controllers/adminController')
+const productsController = require('./controllers/productsController')
 
 router.use('/users',userController)
 router.use('/admin',isAdmin,adminController)
+router.use('/products',productsController)
+
 
 router.all('*', (req,res) => {
     res.status(404).json({error:'This service does not exist!'})
