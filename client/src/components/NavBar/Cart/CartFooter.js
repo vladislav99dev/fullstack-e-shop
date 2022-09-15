1f68370c3347a73b911a8a246e68a9b743aa5641
vladislav99dev <vladislavcho99@abv.bg>
2022-09-15 12:57:36 +0300

    fix CartFooter

--- client/src/components/NavBar/Cart/CartFooter.js
+++ client/src/components/NavBar/Cart/CartFooter.js
@@ -1,5 +1,8 @@
+
+
 const CartFooter = ({
-totalPrice
+totalPrice,
+toggleCartMenu
 }) => {
     return (
         <div className="border-t border-gray-200 py-6 px-4 sm:px-6">
@@ -9,12 +12,12 @@ totalPrice
         </div>
         <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
         <div className="mt-6">
-          <a href="#" className="flex items-center justify-center rounded-md border border-transparent bg-[#00df9a] px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-green-300">Checkout</a>
+          <a  className="flex items-center justify-center rounded-md border border-transparent bg-[#00df9a] px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-green-300">Checkout</a>
         </div>
         <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
           <p>
             or
-            <button type="button" className="font-medium text-[#00df9a] hover:text-green-300">
+            <button onClick={toggleCartMenu} type="button" className="font-medium text-[#00df9a] hover:text-green-300">
               Continue Shopping
               <span aria-hidden="true"> &rarr;</span>
             </button>
