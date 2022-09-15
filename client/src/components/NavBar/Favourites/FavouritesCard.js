9aed894547be036402c2287b34caa2f081c9a1e3
vladislav99dev <vladislavcho99@abv.bg>
2022-09-15 18:46:34 +0300

    add favourites component and separete iinto smaller ones

---
+++ client/src/components/NavBar/Favourites/FavouritesCard.js
@@ -0,0 +1,67 @@
+import { removeFromFavourties } from "../../../services/favouritesAndCartServices";
+import useModalState from "../../../hooks/useModalState";
+import { useAuthContext } from "../../../context/AuthContext";
+import AttentionModal from "../../Modals/AttentionModal";
+import { Link } from "react-router-dom";
+
+const FavouritesCard = ({
+  product,
+  profileId,
+  manageIsLoading,
+  toggleFavouritesMenu
+}) => {
+    const {modalState,setFailedModal,resetModals} = useModalState();
+    const {login} = useAuthContext();
+    
+    const removeHandler = async(event) => {
+      event.preventDefault();
+      manageIsLoading(true)
+      try{
+        const response = await removeFromFavourties(profileId,product._id);
+        const jsonResponse = await response.json();
+        if(response.status !== 200) setFailedModal(jsonResponse.message);
+        if(response.status === 200) login(jsonResponse.user);
+        manageIsLoading(false)
+      }catch(err){
+        console.log(err);
+        manageIsLoading(false)
+      }
+    }
+    return (
+      <>
+      {modalState.isFailed.value 
+      ? <AttentionModal
+      titleMessage={'Something went wrong'}
+      descriptionMessage={modalState.isFailed.message}
+      buttonHandler={resetModals}
+      buttonName={"Try again"}/>
+      : null
+      }
+        <li className="flex py-6">
+        <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+          <img src={product.imageUrl} alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt." className="h-full w-full object-cover object-center"/>
+        </div>
+
+        <div className="ml-4 flex flex-1 flex-col">
+          <div>
+            <div className="flex justify-between text-base font-medium text-gray-900">
+              <h3 onClick={toggleFavouritesMenu}>
+                <Link to={`products/${product.gender}/details/${product._id}`}>{product.name}</Link>
+              </h3>
+              <p className="ml-4">{`$${product.price}`}</p>
+            </div>
+            <p className="mt-1 text-md text-gray-500 font-bold capitalize">{product.brand}</p>
+            <p className="mt-1 text-sm text-gray-500 capitalize">{product.color}</p>
+          </div>
+          <div className="flex flex-1 items-end justify-end text-sm">
+            <div className="flex">
+              <button onClick={removeHandler} type="button" className="font-medium text-[#00df9a] hover:text-green-300 ">Remove</button>
+            </div>
+          </div>
+        </div>
+      </li>
+      </>
+    )
+}
+
+export default FavouritesCard;
---
+++ client/src/components/NavBar/Favourites/FavouritesLayout.js
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import { useNavTogglesContext } from "../../../context/NavTogglesContext";
+import { useAuthContext } from "../../../context/AuthContext";
+
+import FavouritesCard from "./FavouritesCard";
+import FavouritesFooter from "./FavouritesFooter";
+import FavouritesHeader from "./FavouritesHeader";
+import Spinner from "../../Spinner/Spinner";
+
+const FavouritesLayout = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { toggleFavouritesMenu } = useNavTogglesContext();
+  const { user } = useAuthContext();
+
+  const manageIsLoading = (value) => {
+    setIsLoading(value);
+  };
+
+  return (
+    <div>
+      {/* // <!-- This example requires Tailwind CSS v2.0+ --> */}
+      <div
+        className="relative z-10"
+        aria-labelledby="slide-over-title"
+        role="dialog"
+        aria-modal="true"
+      >
+        {/* //   <!--
+        //     Background backdrop, show/hide based on slide-over state.
+        
+        //     Entering: "ease-in-out duration-500"
+        //       From: "opacity-0"
+        //       To: "opacity-100"
+        //     Leaving: "ease-in-out duration-500"
+        //       From: "opacity-100"
+        //       To: "opacity-0"
+        //   --> */}
+        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+        <div className="fixed inset-0 overflow-hidden">
+          <div className="absolute inset-0 overflow-hidden">
+            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+              {/* // <!--
+                //   Slide-over panel, show/hide based on slide-over state.
+        
+                //   Entering: "transform transition ease-in-out duration-500 sm:duration-700"
+                //     From: "translate-x-full"
+                //     To: "translate-x-0"
+                //   Leaving: "transform transition ease-in-out duration-500 sm:duration-700"
+                //     From: "translate-x-0"
+                //     To: "translate-x-full"
+                // --> */}
+              <div className="pointer-events-auto w-screen max-w-md">
+                <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                  <div className="flex-1 overflow-y-auto py-6 px-4 sm:px-6">
+                    <FavouritesHeader
+                      toggleFavouritesMenu={toggleFavouritesMenu}
+                    />
+                    <div className="mt-8">
+                      <div className="flow-root">
+                        <ul
+                          role="list"
+                          className="-my-6 divide-y divide-gray-200"
+                        >{isLoading 
+                        ? <Spinner/>
+                        : user.favourites.map((favourite) => (
+                          <FavouritesCard
+                            key={favourite._id}
+                            product={favourite}
+                            profileId = {user._id}
+                            manageIsLoading={manageIsLoading}
+                            toggleFavouritesMenu={toggleFavouritesMenu}
+                          />
+                        ))
+                        }
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <FavouritesFooter />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FavouritesLayout;
