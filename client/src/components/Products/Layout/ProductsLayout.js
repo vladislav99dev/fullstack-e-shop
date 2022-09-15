20e327e9c3b16d6fef3a403b5fca6ac0cac9c51a
vladislav99dev <vladislavcho99@abv.bg>
2022-09-15 13:00:16 +0300

    ProductsLayout work with 'all' products  and add spinner

--- client/src/components/Products/Layout/ProductsLayout.js
+++ client/src/components/Products/Layout/ProductsLayout.js
@@ -3,20 +3,25 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useEffect,useState } from "react";
 import * as productsRequester from "../../../services/productsRequester"
 import useModalState from "../../../hooks/useModalState";
+import AttentionModal from "../../Modals/AttentionModal";
+import Spinner from "../../Spinner/Spinner";
 
 const ProductsLayout = () => {
   const {gender} = useParams();
   const navigate = useNavigate();
   const [products,setProducts] = useState([]);
+  const [isLoading,setIsLoading] = useState(true);
   const {modalState,setSuccessModal,setFailedModal,resetModals} = useModalState()
 
 
   useEffect(()=> {
-    if(gender !== 'men' && gender !== 'women' && gender !== 'boys' && gender !== 'girls') navigate('/users/logout');
-    console.log(gender);
+    setIsLoading(true)
+    if(gender !== 'men' && gender !== 'women' && gender !== 'boys' && gender !== 'girls' && gender !== 'all') navigate('/');
     initialRequest(gender).then(({response,jsonResponse}) => {
+      setIsLoading(false)
       if (response.status !== 200) setFailedModal(jsonResponse.message);
       if (response.status === 200) setProducts(jsonResponse)
+
     }).catch((err) => {
       setFailedModal("Server time out.");
     })
@@ -30,19 +35,27 @@ const ProductsLayout = () => {
   }
 
   return (
-    //should isplay attention modal
-
-    <div className="bg-white">
-      <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-[100rem] lg:px-8">
+    <>
+    {modalState.isFailed.value 
+    ? <AttentionModal 
+    titleMessage={"Something went wrong"}
+    descriptionMessage={modalState.isFailed.message}
+    buttonHandler={resetModals}
+    buttonName={"Try again"}/>
+    :<div className="bg-white">
+    { isLoading 
+    ? <Spinner/>
+    : <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-[100rem] lg:px-8">
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">
           Customers also purchased
         </h2>
-
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
             {products.map(product  => <ProductLayoutCard key={product._id} product={product}/>)}
         </div>
-      </div>
+      </div>}
     </div>
+    }
+  </>  
   );
 };
 
