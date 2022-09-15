65a8754a40d503c050ffa54850d47adfd678118b
vladislav99dev <vladislavcho99@abv.bg>
2022-09-15 12:54:28 +0300

    add support for all products view in controller

--- api/controllers/productsController.js
+++ api/controllers/productsController.js
@@ -5,11 +5,14 @@ const productsServices = require('../services/productsServices')
 
 const getManyHandler = async(req,res) => {
     console.log(`GET ${req.originalUrl}`);
-    console.log('something');
     const gender = req.path.split('/')[1];
     try{
-        if(gender !== 'men' && gender !== 'women' && gender !=='boys' && gender !== 'girls'){
+        if(gender !== 'men' && gender !== 'women' && gender !=='boys' && gender !== 'girls' && gender !== 'all'){
             throw {status:400, message:'No such gender found in database!'}
+        }
+        if(gender === 'all') {
+            const dbResponse = await productsServices.findAll();
+            return res.status(200).json(dbResponse);
         }
         const dbResponse = await productsServices.findByGender(gender);
         res.status(200).json(dbResponse);
@@ -33,7 +36,7 @@ const getOneHandler = async(req,res) => {
 
 
 
-router.get(["/men","/women","/girls","/boys"], getManyHandler)
+router.get(["/men","/women","/girls","/boys","/all"], getManyHandler)
 router.get("/:productId", getOneHandler)
 
 module.exports = router; 
