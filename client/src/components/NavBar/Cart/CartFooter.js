cd1027f49455a219b950d543c4390b19277f7045
vladislav99dev <vladislavcho99@abv.bg>
2022-09-16 15:52:31 +0300

    add click handler for checkout btn

--- client/src/components/NavBar/Cart/CartFooter.js
+++ client/src/components/NavBar/Cart/CartFooter.js
@@ -1,9 +1,16 @@
-
+import { useNavigate } from "react-router-dom";
 
 const CartFooter = ({
 totalPrice,
 toggleCartMenu
 }) => {
+  const navigate = useNavigate();
+
+  const checkoutHandler = (event) => {
+    event.preventDefault();
+    toggleCartMenu();
+    navigate('/order/checkout')
+  }
     return (
         <div className="border-t border-gray-200 py-6 px-4 sm:px-6">
         <div className="flex justify-between text-base font-medium text-gray-900">
@@ -12,7 +19,7 @@ toggleCartMenu
         </div>
         <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
         <div className="mt-6">
-          <a  className="flex items-center justify-center rounded-md border border-transparent bg-[#00df9a] px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-green-300">Checkout</a>
+          <button onClick={checkoutHandler} className="flex items-center justify-center rounded-md border border-transparent w-full bg-[#00df9a] px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-green-300">Checkout</button>
         </div>
         <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
           <p>
