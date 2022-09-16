e066465a1b7dec4e0d8e0c1baba9fca4a0531503
vladislav99dev <vladislavcho99@abv.bg>
2022-09-16 20:16:14 +0300

    add tiger icon to mobile navigations

--- client/src/components/NavBar/MobileNavBar.js
+++ client/src/components/NavBar/MobileNavBar.js
@@ -4,6 +4,7 @@ import { RiShoppingCart2Fill, RiUserAddFill } from "react-icons/ri";
 import { MdFavorite } from "react-icons/md";
 import { IoClose } from "react-icons/io5";
 import { ImMenu } from "react-icons/im";
+import {GiTigerHead} from "react-icons/gi"
 
 import UserLinks from "./UserLinks";
 
@@ -13,12 +14,10 @@ const MobileProductsLinks = () => {
   const { toggleProductsMenu } = useNavTogglesContext();
   return (
     <>
-      <div className="h-24 mt-4 flex lg:hidden">
-        <h1 className="w-3/4 h-full text-3xl text-[#00df9a] font-bold ml-4 drop-shadow-xl">
-          Supreme
-          <br />
-          Fashion Shop
-        </h1>
+      <div className="h-24 mt-4 flex justify-around lg:hidden">
+        <div className="flex">
+          <GiTigerHead className="mt-2 mr-32" size={80} color={'white'}/>
+        </div>
         <IoClose
           size={50}
           color={"00df9a"}
@@ -74,12 +73,8 @@ export const MobileUserLinks = () => {
   const { toggleUserMenu } = useNavTogglesContext();
   return (
     <>
-      <div className="h-24 mt-4 flex lg:hidden">
-        <h1 className="w-3/4 h-full text-3xl text-[#00df9a] font-bold ml-4">
-          Supreme
-          <br />
-          Fashion Shop
-        </h1>
+      <div className="h-24 mt-4 flex justify-around lg:hidden">
+        <GiTigerHead className="mt-2 mr-32" size={80} color={'white'}/>
         <IoClose
           size={50}
           color={"00df9a"}
@@ -97,13 +92,16 @@ export const MobileUserLinks = () => {
 const MobileNavBar = () => {
   const { toggleUserMenu, toggleProductsMenu,toggleCartMenu } = useNavTogglesContext();
   return (
-    <div className="flex w-full mt-4 lg:hidden">
-      <h1 className="text-3xl font-serif italic drop-shadow-xl text-[#00df9a] mt-4 w-1/2 xs:ml-4 md:ml-8 ">
+    <div className="flex justify-around w-full mt-4 lg:hidden">
+      <div className="flex">
+      <GiTigerHead className="mt-4 ml-6" size={80} color={'white'}/>
+      {/* <h1 className="text-3xl font-serif italic drop-shadow-xl text-[#00df9a] mt-4 w-1/2 xs:ml-4 md:ml-8 ">
         Supreme
         <br />
         Fashion Shop
-      </h1>
-      <div className="flex ml-4 h-16 mt-8 xs:ml-32 sm:ml-32 mr-2 md:ml-52">
+      </h1> */}
+      </div>
+      <div className="flex ml-4 h-16 mt-8 xs:ml-32 sm:ml-20 mr-2 md:ml-52">
         <Link className="px-2" to="/favorite">
           <MdFavorite size={35} color={"#00df9a"} className="mt-4" />
         </Link>
