aa1e5d01a9d611177df5b0716c1cd99160ab523e
vladislav99dev <vladislavcho99@abv.bg>
2022-09-17 20:52:07 +0300

    remove all functionallity form CartCard and move it to parent

--- client/src/components/NavBar/Cart/CartCard.js
+++ client/src/components/NavBar/Cart/CartCard.js
@@ -1,48 +1,14 @@
 import { Link } from "react-router-dom";
-import { removeFromCart } from "../../../services/favouritesAndCartServices";
-import { useAuthContext } from "../../../context/AuthContext";
-import useModalState from "../../../hooks/useModalState";
-import AttentionModal from "../../Modals/AttentionModal"
 
 const CartCard = ({
     product,
     quantity,
     size,
     toggleCartMenu,
-    profileId,
-    manageIsLoading
+    removeHandler,
 }) => {
-
-
-  const {modalState,setFailedModal,resetModals} = useModalState();
-  const {login} = useAuthContext();
-
-
-  const removeHandler = async(event) => {
-    event.preventDefault();
-    manageIsLoading(true)
-    try{
-      const response = await removeFromCart(profileId,product._id,size)
-      const jsonResponse = await response.json();
-      if(response.status !== 200) setFailedModal(jsonResponse.message);
-      if(response.status === 200) login(jsonResponse.user);
-      manageIsLoading(false)
-    }catch(err){
-      console.log(err);
-      manageIsLoading(false)
-    }
-
-  }
     return(
       <>
-      {modalState.isFailed.value 
-      ? <AttentionModal
-      titleMessage={'Something went wrong'}
-      descriptionMessage={modalState.isFailed.message}
-      buttonHandler={resetModals}
-      buttonName={"Try again"}/>
-      :null
-      }
         <li className="flex py-6 hover:bg-gray-100 rounded-md ease-in duration-100">
         <div className="h-36 w-36 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
           <img src={product.imageUrl} alt="Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch." className="h-full w-full object-cover object-center"/>
--- client/src/components/NavBar/Cart/CartLayout.js
+++ client/src/components/NavBar/Cart/CartLayout.js
@@ -3,8 +3,12 @@ import CartCard from "./CartCard";
 import CartFooter from "./CartFooter";
 import CartHeader from "./CartHeader";
 import Spinner from "../../Spinner/Spinner";
+import { removeFromCart } from "../../../services/favouritesAndCartServices";
 import { useAuthContext } from "../../../context/AuthContext";
 import { useNavTogglesContext } from "../../../context/NavTogglesContext";
+import { useLocalProductsContext } from "../../../context/LocalProductsContext";
+import useModalState from "../../../hooks/useModalState";
+import AttentionModal from "../../Modals/AttentionModal";
 
 
 
@@ -12,31 +16,63 @@ const ProductsCard = () => {
 
   const [isLoading,setIsLoading] = useState(false);
   const {toggleCartMenu} = useNavTogglesContext();
-  const { user } = useAuthContext();
+  const { user,login } = useAuthContext();
+  const { products, removeProduct} = useLocalProductsContext();
+  const {modalState,setFailedModal,resetModals} = useModalState();
+
   let totalPrice = 0;
-  user.cart.map((product) => totalPrice += product._id.price * product.quantity);
+  // user.cart.map((product) => totalPrice += product._id.price * product.quantity);
 
   const manageIsLoading = (value) => {
     setIsLoading(value)
   }
 
+  const removeProductFromStorage = (product,size,event) => {
+    event.preventDefault();
+    removeProduct(product,size);
+  }
 
+  const removeFromCartHandler = async(productId,size,event) => {
+    event.preventDefault();
+    manageIsLoading(true)
+    try{
+      const response = await removeFromCart(user._id,productId,size)
+      const jsonResponse = await response.json();
+      console.log(response);
+      if(response.status !== 200) setFailedModal(jsonResponse.message);
+      if(response.status === 200) login(jsonResponse.user);
+      manageIsLoading(false)
+    }catch(err){
+      console.log(err);
+      manageIsLoading(false)
+    }
+
+  }
   return (
+    <>
     <div
       className="relative z-10"
       aria-labelledby="slide-over-title"
       role="dialog"
       aria-modal="true"
     >
+      {modalState.isFailed.value 
+      ? <AttentionModal
+      titleMessage={'Something went wrong'}
+      descriptionMessage={modalState.isFailed.message}
+      buttonHandler={resetModals}
+      buttonName={"Try again"}/>
+      :null
+      }
       {/* <!--
     Background backdrop, show/hide based on slide-over state.
-
+    
     Entering: "ease-in-out duration-500"
-      From: "opacity-0"
-      To: "opacity-100"
+    From: "opacity-0"
+    To: "opacity-100"
     Leaving: "ease-in-out duration-500"
-      From: "opacity-100"
-      To: "opacity-0"
+    From: "opacity-100"
+    To: "opacity-0"
   --> */}
       <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 
@@ -45,13 +81,13 @@ const ProductsCard = () => {
           <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
             {/* <!--
           Slide-over panel, show/hide based on slide-over state.
-
+          
           Entering: "transform transition ease-in-out duration-500 sm:duration-700"
-            From: "translate-x-full"
-            To: "translate-x-0"
+          From: "translate-x-full"
+          To: "translate-x-0"
           Leaving: "transform transition ease-in-out duration-500 sm:duration-700"
-            From: "translate-x-0"
-            To: "translate-x-full"
+          From: "translate-x-0"
+          To: "translate-x-full"
         --> */}
             <div className="pointer-events-auto w-screen max-w-md">
               <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
@@ -63,22 +99,62 @@ const ProductsCard = () => {
                       <ul
                         role="list"
                         className="-my-6 divide-y divide-gray-200"
-                      >{isLoading 
+                        >
+                        {isLoading ? <Spinner/> : null}
+
+                        {products.length > 0 
+                        ? products.map(({product,quantity,size}) => (
+                          <CartCard 
+                          key={`${product._id}${product.size}`}
+                          product={product}
+                          quantity={quantity}
+                          size={size}
+                          toggleCartMenu={toggleCartMenu}
+                          removeHandler={removeProductFromStorage.bind(null,product,size)}
+                          />
+                          ))
+                          :null}
+
+                        { user.email ? (user.cart.length > 0 
+                          ? user.cart.map((product) => (
+                            <CartCard
+                            key={`${product._id._id}${product.size}`}
+                            product={product._id}
+                            quantity={product.quantity}
+                            size={product.size}
+                            toggleCartMenu={toggleCartMenu}
+                            removeHandler={removeFromCartHandler.bind(null,product._id._id,product.size)}
+                            />
+                          ))
+                          : null) 
+                          : null 
+                      }
+                  
+                        {/* {isLoading 
                         ? <Spinner/>
                         : user.cart.length 
                         ? user.cart.map((product) => (
-                            <CartCard
-                              key={`${product._id._id}${product.size}`}
-                              product={product._id}
-                              quantity={product.quantity}
-                              size={product.size}
-                              toggleCartMenu={toggleCartMenu}
-                              profileId={user._id}
-                              manageIsLoading={manageIsLoading}
-                            />
+                          <CartCard
+                          key={`${product._id._id}${product.size}`}
+                          product={product._id}
+                          quantity={product.quantity}
+                          size={product.size}
+                          toggleCartMenu={toggleCartMenu}
+                          profileId={user._id}
+                          manageIsLoading={manageIsLoading}
+                          />
                           ))
-                        : <p className="text-md font-bold text-[#00df9a] mt-4">No products added to cart!</p>
-                      }
+                          : <p className="text-md font-bold text-[#00df9a] mt-4">No products added to cart!</p>
+                        } */}
+
+
+
+
+
+
+
+
+
                       </ul>
                     </div>
                   </div>
@@ -90,6 +166,34 @@ const ProductsCard = () => {
         </div>
       </div>
     </div>
+                        </>
   );
 };
 export default ProductsCard;
+
+
+
+
+
+
+
+{/* <ul
+role="list"
+className="-my-6 divide-y divide-gray-200"
+>{isLoading 
+? <Spinner/>
+: user.cart.length 
+? user.cart.map((product) => (
+    <CartCard
+      key={`${product._id._id}${product.size}`}
+      product={product._id}
+      quantity={product.quantity}
+      size={product.size}
+      toggleCartMenu={toggleCartMenu}
+      profileId={user._id}
+      manageIsLoading={manageIsLoading}
+    />
+  ))
+: <p className="text-md font-bold text-[#00df9a] mt-4">No products added to cart!</p>
+}
+</ul> */}
