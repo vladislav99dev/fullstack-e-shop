a4d4462cef64092072df2fae4b95afbfadaf6234
vladislav99dev <vladislavcho99@abv.bg>
2022-09-18 13:11:29 +0300

    change login form styles

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link,  useNavigate } from "react-router-dom";
+import { Link,  Navigate,  useNavigate } from "react-router-dom";
 
 import { validateLogin } from "../../services/formValidationsServices";
 import ValidationMessage from "../ValidationMessage/validationMessage";
@@ -52,33 +52,22 @@ const Login = () => {
 
 
     return(
-        <div className="bg-[#FAF9F6]  rounded-3xl lg:mt-16 w-full shadow-lg flex-row lg:w-full">
+        <div className="bg-white pt-6 pb-10  rounded-3xl lg:mt-16 w-full shadow-lg flex-row lg:w-full">
             {isLoading 
             ?<Spinner/>
-            :<><h1 className="text-[#00df9a] text-2xl italic uppercase font-bold w-full text-center mt-8">Login</h1>
+            :<><h1 className="text-[#00df9a] text-2xl italic uppercase font-bold w-full text-center">Login</h1>
             {messages.length > 0
             ? messages.map((message) => <ValidationMessage key={message} message={message}/>)
             : null
             }
             <form onSubmit={loginHandler}>
-            <div className="flex mt-10 justify-center">
-                <label htmlFor="email">Email:</label>
-                <input type="input" name="email" id="email" className="ml-8" placeholder="ex.petrpetrov@abv.bg" />
-            </div>
-            <div className="flex mt-4 justify-center">
-                <label htmlFor="password">Password:</label>
-                <input type="password" name="password" id="password"  />
-            </div>
-            <div className="flex justify-center mt-10">
-                <label htmlFor="rememberMe">Remember me?</label>
-                <input className="ml-1" type="checkbox" name="rememberMe"/>
-            </div> 
-            <div className="flex justify-center mt-2">
-                <label>Don't have an account?<Link to={"users/register"} className="text-[#3abeff] text-lg"> Sign up</Link></label>
-            </div> 
-            <div className="flex justify-center mt-6 ">
-                <button type="submit" className="py-2 mb-10 px-10 rounded-md text-white bg-[#DDDDDD] font-bold">Submit</button>
-            </div>
+                <input type="text" name="email" className="py-2 w-2/3 ml-[17%] md:w-1/3 md:ml-[33.5%] mb-2 mt-6 border-2 border-green-300 hover:border-green-100 rounded-md" placeholder="Email:vladislavdorovski@abv.bg" />
+                <input type="password" name="password" className="py-2 w-2/3 ml-[17%] md:w-1/3 md:ml-[33.5%] border-2 border-green-300 hover:border-green-100 rounded-md" placeholder="Password:" />
+                <div className="w-2/3 ml-[17%] md:w-1/3 md:ml-[33.5%] flex justify-around mt-4">
+                    <p className="italic text-gray-600">You dont have an account?</p>
+                    <Link to={'users/register'} className="text-[#00df9a] font-bold">Sign in</Link>
+                </div>
+                <button className="py-2  border-[#00df9a] w-[25%] ml-[37.5%] mt-4 rounded-md italic font-bold text-xl text-white bg-[#00df9a] hover:bg-green-300 ease-in-out duration-500">Submit</button>
             </form>
             </>
             }
@@ -89,3 +78,22 @@ const Login = () => {
 
 
 export default isNotLoggedIn(Login);
+
+{/* <div className="flex mt-10 justify-center">
+<label htmlFor="email">Email:</label>
+<input type="input" name="email" id="email" className="ml-8" placeholder="ex.petrpetrov@abv.bg" />
+</div>
+<div className="flex mt-4 justify-center">
+<label htmlFor="password">Password:</label>
+<input type="password" name="password" id="password"  />
+</div>
+<div className="flex justify-center mt-10">
+<label htmlFor="rememberMe">Remember me?</label>
+<input className="ml-1" type="checkbox" name="rememberMe"/>
+</div> 
+<div className="flex justify-center mt-2">
+<label>Don't have an account?<Link to={"users/register"} className="text-[#3abeff] text-lg"> Sign up</Link></label>
+</div> 
+<div className="flex justify-center mt-6 ">
+<button type="submit" className="py-2 mb-10 px-10 rounded-md text-white bg-[#DDDDDD] font-bold">Submit</button>
+</div> */}
