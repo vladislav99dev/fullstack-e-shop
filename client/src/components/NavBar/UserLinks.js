8aab92667cedd97fefe0bec50625db96c6605ec8
vladislav99dev <vladislavcho99@abv.bg>
2022-09-18 13:09:59 +0300

    change style of user links

--- client/src/components/NavBar/UserLinks.js
+++ client/src/components/NavBar/UserLinks.js
@@ -15,15 +15,15 @@ const UserLinks = ({
             <div className="container-links">
                 <Link to={"users/login"} className="mobile-links" onClick={clickHandler} >Login</Link>
             </div>
-            <div className="container-links">
-                <Link to={"users/register"} className="mobile-links" onClick={clickHandler}>Register</Link>
+            <div className="container-links border-0">
+                <Link to={"users/register"} className="mobile-links " onClick={clickHandler}>Register</Link>
             </div>
         </>
         :<>
             <div className="container-links">
                 <Link to={"users/logout"} className="mobile-links" onClick={clickHandler}>Logout</Link>
             </div>
-            <div className="container-links">
+            <div className="container-links border-0">
                 <Link to={"#"} className="mobile-links" onClick={clickHandler}>Edit Profile</Link>
             </div>
         </>
