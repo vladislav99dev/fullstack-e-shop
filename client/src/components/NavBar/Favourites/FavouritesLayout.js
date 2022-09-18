d312b08fd1b2da306acbd06e77782437f0a8b0a7
vladislav99dev <vladislavcho99@abv.bg>
2022-09-18 13:10:27 +0300

    add Modal if not logged in user clicks on favourites icon

--- client/src/components/NavBar/Favourites/FavouritesLayout.js
+++ client/src/components/NavBar/Favourites/FavouritesLayout.js
@@ -1,16 +1,19 @@
 import { useState } from "react";
 import { useNavTogglesContext } from "../../../context/NavTogglesContext";
 import { useAuthContext } from "../../../context/AuthContext";
+import useModalState from "../../../hooks/useModalState";
 
 import FavouritesCard from "./FavouritesCard";
 import FavouritesFooter from "./FavouritesFooter";
 import FavouritesHeader from "./FavouritesHeader";
 import Spinner from "../../Spinner/Spinner";
+import AttentionModal from "../../Modals/AttentionModal";
 
 const FavouritesLayout = () => {
   const [isLoading, setIsLoading] = useState(false);
   const { toggleFavouritesMenu } = useNavTogglesContext();
   const { user } = useAuthContext();
+  const {resetModals} = useModalState(); 
 
   const manageIsLoading = (value) => {
     setIsLoading(value);
@@ -19,71 +22,80 @@ const FavouritesLayout = () => {
   return (
     <div>
       {/* // <!-- This example requires Tailwind CSS v2.0+ --> */}
-      <div
-        className="relative z-10"
-        aria-labelledby="slide-over-title"
-        role="dialog"
-        aria-modal="true"
-      >
-        {/* //   <!--
-        //     Background backdrop, show/hide based on slide-over state.
-        
-        //     Entering: "ease-in-out duration-500"
-        //       From: "opacity-0"
-        //       To: "opacity-100"
-        //     Leaving: "ease-in-out duration-500"
-        //       From: "opacity-100"
-        //       To: "opacity-0"
-        //   --> */}
-        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+      {!user.email 
+      ? <AttentionModal
+      titleMessage={"Something went wrong"}
+      descriptionMessage={"You should be logged in to have access to favourites!"}
+      buttonHandler={resetModals}
+      buttonName={"Try again"}
+      />  
+      :<div
+      className="relative z-10"
+      aria-labelledby="slide-over-title"
+      role="dialog"
+      aria-modal="true"
+    >
+      {/* //   <!--
+      //     Background backdrop, show/hide based on slide-over state.
+      
+      //     Entering: "ease-in-out duration-500"
+      //       From: "opacity-0"
+      //       To: "opacity-100"
+      //     Leaving: "ease-in-out duration-500"
+      //       From: "opacity-100"
+      //       To: "opacity-0"
+      //   --> */}
+      <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 
-        <div className="fixed inset-0 overflow-hidden">
-          <div className="absolute inset-0 overflow-hidden">
-            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
-              {/* // <!--
-                //   Slide-over panel, show/hide based on slide-over state.
-        
-                //   Entering: "transform transition ease-in-out duration-500 sm:duration-700"
-                //     From: "translate-x-full"
-                //     To: "translate-x-0"
-                //   Leaving: "transform transition ease-in-out duration-500 sm:duration-700"
-                //     From: "translate-x-0"
-                //     To: "translate-x-full"
-                // --> */}
-              <div className="pointer-events-auto w-screen max-w-md">
-                <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
-                  <div className="flex-1 overflow-y-auto py-6 px-4 sm:px-6">
-                    <FavouritesHeader
-                      toggleFavouritesMenu={toggleFavouritesMenu}
-                    />
-                    <div className="mt-8">
-                      <div className="flow-root">
-                        <ul
-                          role="list"
-                          className="-my-6 divide-y divide-gray-200"
-                        >{isLoading 
-                        ? <Spinner/>
-                        : user.favourites.map((favourite) => (
-                          <FavouritesCard
-                            key={favourite._id}
-                            product={favourite}
-                            profileId = {user._id}
-                            manageIsLoading={manageIsLoading}
-                            toggleFavouritesMenu={toggleFavouritesMenu}
-                          />
-                        ))
-                        }
-                        </ul>
-                      </div>
+      <div className="fixed inset-0 overflow-hidden">
+        <div className="absolute inset-0 overflow-hidden">
+          <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+            {/* // <!--
+              //   Slide-over panel, show/hide based on slide-over state.
+      
+              //   Entering: "transform transition ease-in-out duration-500 sm:duration-700"
+              //     From: "translate-x-full"
+              //     To: "translate-x-0"
+              //   Leaving: "transform transition ease-in-out duration-500 sm:duration-700"
+              //     From: "translate-x-0"
+              //     To: "translate-x-full"
+              // --> */}
+            <div className="pointer-events-auto w-screen max-w-md">
+              <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                <div className="flex-1 overflow-y-auto py-6 px-4 sm:px-6">
+                  <FavouritesHeader
+                    toggleFavouritesMenu={toggleFavouritesMenu}
+                  />
+                  <div className="mt-8">
+                    <div className="flow-root">
+                      <ul
+                        role="list"
+                        className="-my-6 divide-y divide-gray-200"
+                      >{isLoading 
+                      ? <Spinner/>
+                      : user.favourites.map((favourite) => (
+                        <FavouritesCard
+                          key={favourite._id}
+                          product={favourite}
+                          profileId = {user._id}
+                          manageIsLoading={manageIsLoading}
+                          toggleFavouritesMenu={toggleFavouritesMenu}
+                        />
+                      ))
+                      }
+                      </ul>
                     </div>
                   </div>
-                  <FavouritesFooter />
                 </div>
+                <FavouritesFooter />
               </div>
             </div>
           </div>
         </div>
       </div>
+    </div>
+    }
+      
     </div>
   );
 };
