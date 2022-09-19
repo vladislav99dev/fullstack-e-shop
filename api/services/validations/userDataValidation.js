365547a54ef40bae9788ad2e5df6e1fc9e63c5a1
vladislav99dev <vladislavcho99@abv.bg>
2022-09-20 02:47:06 +0300

    add passwordValidation in userDataValidations

--- api/services/validations/userDataValidation.js
+++ api/services/validations/userDataValidation.js
@@ -3,18 +3,18 @@ const validateIsAllDataSend = (data) => {
       !data.firstName ||
       !data.lastName ||
       !data.email ||
+      !data.password ||
       !data.country ||
       !data.city ||
       !data.street ||
       !data.state ||
       !data.zipCode ||
       !data.unitNumber ||
-      !data.phoneNumber ||
-      !data.productsOrdered
+      !data.phoneNumber
     )
       throw {
         message:
-          "Type, category, brand, gender, imageUrl, color, price, sizes should be provided in order to continue!",
+          "FisrtName, LastName, Password, Email, Country, City, Street, State, Zip Code, Unit Number, Phone Number,need to be provided in order to continue!",
       };
   };
   
@@ -35,21 +35,31 @@ const validateEmail = (email) => {
       throw { message: "Email is not in valid format." };
     }
   };
+
+const validatePassword = (password) => {
+    if (!/[0-9]/.test(password)) {
+      throw { message: "Password is not in valid format." };
+    }
+  };
+
 const validateCountry = (country) => {
     if (!/^[a-z -']+$/i.test(country)) {
       throw { message: "Country is not in valid format." };
     }
   };
+
 const validateCity = (city) => {
     if (!/^[a-z -']+$/i.test(city)) {
       throw { message: "City is not in valid format." };
     }
   };
+
 const validateStreet = (street) => {
     if (!/^[a-z -'0-9]+$/i.test(street)) {
       throw { message: "Street is not in valid format." };
     }
   };
+
 const validateState = (state) => {
     if (!/[A-Za-z]+$/.test(state)) {
       throw { message: "State is not in valid format." };
@@ -61,11 +71,13 @@ const validateZipCode = (zipCode) => {
       throw { message: "Zip Code is not in valid format." };
     }
   };
+
 const validateUnitNumber = (unitNumber) => {
     if (!/[0-9]+$/gm.test(unitNumber)) {
       throw { message: "Unit Number is not in valid format." };
     }
   };
+
 const validatePhoneNumber = (phoneNumber) => {
     if (!/[0-9]+$/i.test(phoneNumber)) {
       throw { message: "Phone Number is not in valid format." };
@@ -73,10 +85,12 @@ const validatePhoneNumber = (phoneNumber) => {
   };
 
 const validateAllData = (data) => {
+  try{
     validateIsAllDataSend(data)
     validateFirstName(data.firstName);
     validateLastName(data.lastName);
     validateEmail(data.email);
+    validatePassword(data.password)
     validateCountry(data.country);
     validateCity(data.city);
     validateStreet(data.street);
@@ -84,6 +98,10 @@ const validateAllData = (data) => {
     validateZipCode(data.zipCode);
     validateUnitNumber(data.unitNumber);
     validatePhoneNumber(data.phoneNumber);
+  } catch(err){
+    throw {type:'validation', message:err.message}
+  }
+
   };
 
   const userDataValidation ={
