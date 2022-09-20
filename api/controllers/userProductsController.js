4b5f5dc5cb9c0d841ba960eab447ae12c26e9249
vladislav99dev <vladislavcho99@abv.bg>
2022-09-20 21:29:35 +0300

    make adding products to User;s Cart much cleaner

--- api/controllers/userProductsController.js
+++ api/controllers/userProductsController.js
@@ -5,59 +5,147 @@ const userServices = require('../services/userServices')
 
 const isLoggedIn = require('../middlewares/isLoggedIn');
 
-const addHandler = async(service,req,res) => {
+
+
+const isProfileIdValid = async(profileid) => {
+    const user = await userServices.findById(profileid);
+    if(!user) throw {status:400, message:'There is no user with this id!'};
+    return user;
+}
+
+const isProductIdValid = async(productId) => {
+    const product = await productsServices.getOne(productId);
+    if(!product) throw {status:400, message:'There is no product with this id!'};
+    return product;
+}
+
+const updateAndPopulateUserProfile = async(user) => {
+    const updatedUser = await userServices.findByIdAndUpdate(user._id,user);
+    const {password, ...populatedUser} = await userServices.findByIdAndPopulate(updatedUser._id);
+    return populatedUser;
+}
+
+const isSizeValid = (product,size) => {
+    if(!product.sizes.hasOwnProperty(size)) throw {status:400, message:'You have entered invalid size!'}
+}
+
+const removeOldCartProduct = (user,product) =>{
+    const indexOfProduct = user["cart"].indexOf(product);
+    user["cart"].splice(indexOfProduct,1);
+    return user;
+}
+
+const productWithSameSizeHandler = (user,productId,size,quantity) => {
+    for (const cartProduct of user["cart"]) {
+        if(String(cartProduct._id).includes(productId) && cartProduct.size === size){
+            const incrementedQtyProduct = Object.assign({},{
+                size:cartProduct.size,
+                quantity:cartProduct.quantity + quantity,
+                _id:cartProduct._id
+            })
+
+            const updatedUser = removeOldCartProduct(user,cartProduct);
+            updatedUser["cart"].push(incrementedQtyProduct);
+            return {isUpdated:true, profile:updatedUser};
+        }
+    }
+    return {isUpdated:false, profile:user};
+}
+
+
+const addToCartHandler = async(req,res) => {
     console.log(`POST ${req.originalUrl}`);
-    const {profileId, size, quantity} = req.body;
+    const {profileId,size,quantity} = req.body;
     const {productId} = req.params;
-    if(!productId) return res.status(400).json({message:"Product id was not provieded!"})
-    try {
-        const user = await userServices.findById(profileId);
-        if(!user) return res.status(400).json({message: "There is no user with this id!"});
-        
-        const product = await productsServices.getOne(productId);
-        if(!product) return res.status(400).json({message: "There is no product with this id!"});
-        
-        if(service === 'favourites') {
-            if(user[service].includes(productId)) return res.status(409).json({message:`This product is already added to ${service}!`});
-            if(!user[service].includes(productId)) user[service].push(productId);
-        }
 
-        if(service === 'cart') {
-            let newCartProduct = {};
-            let isAlreadyAdded = false
+    try{
+        const user = await isProfileIdValid(profileId);
+        const product = await isProductIdValid(productId);
 
-            if(!product.sizes.hasOwnProperty(size)) return res.status(400).json({message: "You have entered invalid size!"});
+        isSizeValid(product,size);
 
-            
-            for (const orderDetails of user[service]) {
-                if(String(orderDetails._id).includes(productId) && orderDetails.size === size) {
-                    Object.assign(newCartProduct, {size:orderDetails.size, quantity: orderDetails.quantity + quantity,_id:orderDetails._id})
-                    isAlreadyAdded = true;
-                    const foundElementIndex = user[service].indexOf(orderDetails);
-                    user[service].splice(foundElementIndex,1);
-                    break;
-                }
-            }
+        const {isUpdated, profile} = productWithSameSizeHandler(user,productId,size,quantity);
+        if(!isUpdated) profile["cart"].push({_id:productId,size:size,quantity:quantity});
 
-            
-            if(product.sizes[size] < newCartProduct.quantity  || product.sizes[size] < quantity )
-            return res.status(400).json({message: `There are only ${product.sizes[size]} left from ${product.name} ${product.category} from size ${size}!`})
-            
-            if(isAlreadyAdded) user[service].push((newCartProduct))
-            if(!isAlreadyAdded) user[service].push({_id:productId,size:size,quantity:quantity})
-
-        }
-
-        const dbResponse = await userServices.findByIdAndUpdate(user,profileId);
-        const updatedUser = await userServices.findByIdPopulated(profileId);
-        updatedUser.password = null
-        if(!updatedUser) return res.status(400).json({message: "There is no user with this id!"});
-        res.status(200).json({message:`Successfully added ${product.name} ${product.category} from ${size}  to ${service}.`, user:updatedUser});
-    } catch (err) {
-        console.log(err);
-        if(err.path === '_id') return res.status(400).json({message: "One or all of the id's you provided are not in valid format."});
+        const modifiedUser = await updateAndPopulateUserProfile(profile);
+        res.status(200).json({message: `Successfully added ${product.name} ${product.category} from ${size}`, user:modifiedUser})
+    }catch(err){
+        if(err.path === '_id') return res.status(400).json({message: "One or all of the id's you provided are not in valid format."})
+        if(err.status) res.status(err.status).json({message:err.message});
+        console.log(err)
     }
 }
+
+
+
+
+// const addHandler = async(service,req,res) => {
+//     console.log(`POST ${req.originalUrl}`);
+//     const {profileId, size, quantity} = req.body;
+//     const {productId} = req.params;
+
+//     if(!productId) return res.status(400).json({message:"Product id was not provieded!"});
+//     // shoul do the new error handling
+
+//     try {
+//         const user = await userServices.findById(profileId);
+//         if(!user) return res.status(400).json({message: "There is no user with this id!"});
+//     // shoul do the new error handling
+
+        
+//         const product = await productsServices.getOne(productId);
+//         if(!product) return res.status(400).json({message: "There is no product with this id!"});
+//     // shoul do the new error handling
+
+        
+//         if(service === 'favourites') {
+//             if(user[service].includes(productId)) return res.status(409).json({message:`This product is already added to ${service}!`});
+//     // shoul do the new error handling
+
+
+//             if(!user[service].includes(productId)) user[service].push(productId);
+//         }
+
+
+
+
+
+//         if(service === 'cart') {
+//             let newCartProduct = {};
+//             let isAlreadyAdded = false
+
+//             if(!product.sizes.hasOwnProperty(size)) return res.status(400).json({message: "You have entered invalid size!"});
+
+            
+//             for (const orderDetails of user[service]) {
+//                 if(String(orderDetails._id).includes(productId) && orderDetails.size === size) {
+//                     Object.assign(newCartProduct, {size:orderDetails.size, quantity: orderDetails.quantity + quantity,_id:orderDetails._id})
+//                     isAlreadyAdded = true;
+//                     const foundElementIndex = user[service].indexOf(orderDetails);
+//                     user[service].splice(foundElementIndex,1);
+//                     break;
+//                 }
+//             }
+
+            
+//             if(product.sizes[size] < newCartProduct.quantity || product.sizes[size] < quantity )
+//             return res.status(400).json({message: `There are only ${product.sizes[size]} left from ${product.name} ${product.category} from size ${size}!`})
+            
+//             if(isAlreadyAdded) user[service].push((newCartProduct))
+//             if(!isAlreadyAdded) user[service].push({_id:productId,size:size,quantity:quantity})
+
+//         }
+
+//         const dbResponse = await userServices.findByIdAndUpdate(user,profileId);
+//         const updatedUser = await userServices.findByIdAndPopulate(profileId);
+//         updatedUser.password = null
+//         if(!updatedUser) return res.status(400).json({message: "There is no user with this id!"});
+//         res.status(200).json({message:`Successfully added ${product.name} ${product.category} from ${size}  to ${service}.`, user:updatedUser});
+//     } catch (err) {
+//         console.log(err);
+//         if(err.path === '_id') return res.status(400).json({message: "One or all of the id's you provided are not in valid format."});
+//     }
+// }
 
 const removeHandler = async(service,req,res) => {
     console.log(`DELETE ${req.originalUrl}`);
@@ -116,8 +204,7 @@ const getHandler = async(req,res) => {
 };
 
 
-const addFavouriteHandler = addHandler.bind(null,"favourites");
-const addCartHandler = addHandler.bind(null,"cart");
+// const addFavouriteHandler = addHandler.bind(null,"favourites");
 
 const removeFavouriteHandler = removeHandler.bind(null,"favourites")
 const removeCartHandler = removeHandler.bind(null,"cart")
@@ -126,11 +213,11 @@ const removeCartHandler = removeHandler.bind(null,"cart")
 
 
 router.get('/favourites-get', getHandler)
-router.post('/:productId/favourites-add', isLoggedIn, addFavouriteHandler)
+// router.post('/:productId/favourites-add', isLoggedIn, addFavouriteHandler)
 router.delete('/:productId/favourites-remove', isLoggedIn, removeFavouriteHandler)
 
 router.get('/cart-get', getHandler)
-router.post('/:productId/cart-add', isLoggedIn, addCartHandler)
+router.post('/:productId/cart-add', isLoggedIn, addToCartHandler)
 router.delete('/:productId/cart-remove', isLoggedIn, removeCartHandler)
 
 
