1e9e193cd29ee1746ad98748cae09279beccbd33
vladislav99dev <vladislavcho99@abv.bg>
2022-09-22 11:21:05 +0300

    add userValidations to new validation folder

--- client/src/components/Admin/Product/Create.js
+++ client/src/components/Admin/Product/Create.js
@@ -1,19 +1,19 @@
 import { useState } from "react";
-import { useAuthContext } from "../../../context/AuthContext";
 
+import { useAuthContext } from "../../../context/AuthContext";
 import  {useModalsContext} from "../../../context/ModalsContext"
 
 
 import * as productsRequester from '../../../services/productsRequester'
-import {formDataExtracter, dataSizeFormater } from "../../../services/dataServices";
-import productsValidations from "../../../services/formValidations/productsValidations";
+import { productSizeFormater } from "../../../services/dataServices";
+import productsValidations from "../../../validations/productsValidations";
 
 
 import ValidationMessage from "../../ValidationMessage/validationMessage";
 import AttentionModal from "../../Modals/AttentionModal";
 import SuccessModal from "../../Modals/SuccessModal"
 
-import formData from "../../../utils/formData";
+import productData from "../../../utils/productData";
 
 
 
@@ -21,7 +21,7 @@ import formData from "../../../utils/formData";
 const Create = () => {
     const {user} = useAuthContext();
     const [type,setType] = useState('clothing');
-    const [messages,setMessaages] = useState([]);
+    const [validationMessages,setValidationMessages] = useState([]);
     const {modalState, setSuccessModal, setFailedModal, resetModals} = useModalsContext();
 
     const handleSelect = (event) => {
@@ -30,23 +30,26 @@ const Create = () => {
 
     const submitHandler = async(event) => {
         event.preventDefault();
-        const data  = formDataExtracter(event.target)
-        // remove this bullshit
-        console.log(data);
-        const formatedData = dataSizeFormater(data)
+
+        const formData = new FormData(event.target) ;
+        const data = Object.fromEntries(formData);
+
+        const formatedData = productSizeFormater(data)
+
+
         let validationsResponse = productsValidations.validateAllData(formatedData)
-        if(validationsResponse.length > 0) return setMessaages(validationsResponse);
-        if(!validationsResponse.length)  setMessaages([]);
+        if(validationsResponse.length > 0) return setValidationMessages(validationsResponse);
+        if(!validationsResponse.length)  setValidationMessages([]);
+
+
         try{
             const response = await productsRequester.create(formatedData,user.accessToken)
             const jsonResponse = await response.json();
-            if(response.status !== 201){
-                setFailedModal(jsonResponse.message);
-            }
-            if(response.status === 201){
-                setSuccessModal("You have successfully create new product!");
-            }
+
+            if(response.status !== 201) throw {responseStatus:response.status, message:jsonResponse.message}
+            if(response.status === 201) setSuccessModal("You have successfully create new product!");
         } catch(err){
+            if(err.responseStatus) return setFailedModal(err.message);
             console.log(err);
         }
     };
@@ -75,30 +78,30 @@ const Create = () => {
             <br/>
             what product we will create today?
             </h1>
-        {messages.length > 0 
-        ? messages.map((message) => <ValidationMessage key={message} message={message}/>)
+        {validationMessages.length > 0 
+        ? validationMessages.map((message) => <ValidationMessage key={message} message={message}/>)
         : null
         }
         <form onSubmit={submitHandler}>
 
         <select type="text" name="type" onChange={handleSelect} className="form-input capitalize font-semibold text-gray-700" placeholder="">
-            {formData.types.map(type => <option className="text-center font-semibold text-gray-700" key={type} value={type}>{type}</option>)}
+            {productData.types.map(type => <option className="text-center font-semibold text-gray-700" key={type} value={type}>{type}</option>)}
         </select>
 
         <select type="input" name="category" id="category" className="form-input capitalize font-semibold text=gray-700 mt-0">
             {type === 'clothing' 
-            ? formData.clothingOptions.map((option) => <option className="text-center font-semibold text-gray-700" key={option} value={option}>{option}</option>)
-            : formData.shoeOptions.map((option) => <option className="text-center font-semibold text-gray-700"  key={option} value={option}>{option}</option>)
+            ? productData.clothingOptions.map((option) => <option className="text-center font-semibold text-gray-700" key={option} value={option}>{option}</option>)
+            : productData.shoeOptions.map((option) => <option className="text-center font-semibold text-gray-700"  key={option} value={option}>{option}</option>)
 }       </select>
 
         <input type="text" className="form-input mt-0 capitalize text-center font-semibold text-gray-700" name="name" id="name" placeholder="Product Name:"/>
 
         <select type="input" name="gender" id="gender" className="form-input mt-0 capitalize font-semibold text-gray-700">
-        {formData.genders.map(gender => <option className="font-semibold text-gray-700 text-center" key={gender} value={gender}>{gender}</option>)}
+        {productData.genders.map(gender => <option className="font-semibold text-gray-700 text-center" key={gender} value={gender}>{gender}</option>)}
         </select>
 
         <select type="input" name="brand" id="brand" className="form-input mt-0 text-center font-semibold text-grey-700 capitalize">
-        {formData.brands.map(brand => <option className="font-semibold text-gray-700 text-center" key={brand} value={brand}>{brand}</option>)}
+        {productData.brands.map(brand => <option className="font-semibold text-gray-700 text-center" key={brand} value={brand}>{brand}</option>)}
         </select>
 
         <textarea 
@@ -112,7 +115,7 @@ const Create = () => {
         <input type="input" name="imageUrl" id="imageUrl" className="form-input mt-0 text-center font-semibold text-gray-700" placeholder="ImageUrl:" />
         
         <select type="input" name="color" id="color" className="form-input mt-0 text-center font-semibold text-grey-700 capitalize">
-        {formData.colors.map(color => <option className="font-semibold text-gray-700 text-center" key={color} value={color}>{color}</option>)}
+        {productData.colors.map(color => <option className="font-semibold text-gray-700 text-center" key={color} value={color}>{color}</option>)}
         </select>
         
         <input type="number" name="price" id="price" className="form-input mt-0 text-center font-semibold text-gray-700" placeholder="Price:"/>
@@ -126,66 +129,3 @@ const Create = () => {
 
 export default Create;
 
-
-
-
-
-
-
-{/* <div className="flex mt-4 justify-center">
-<label htmlFor="type" className="w-[60px] mr-12">Type:</label>
-<select type="input" name="type" id="type" onChange={handleSelect} className="w-[190px] capitalize">
-{formData.types.map(type => <option key={type} value={type}>{type}</option>)}
-</select>
-</div>
-<div className="flex mt-4 justify-center">
-<label htmlFor="category" className="w-[60px] mr-12">Category:</label>
-<select type="input" name="category" id="category" className="w-[190px] capitalize">
-    {type === 'clothing' 
-    ? formData.clothingOptions.map((option) => <option key={option} value={option}>{option}</option>)
-    : formData.shoeOptions.map((option) => <option key={option} value={option}>{option}</option>)
-}
-</select>
-</div>
-<div className="flex mt-4 justify-center">
-<label htmlFor="name" className="w-[60px] mr-12">Name:</label>
-<input type="input" name="name" id="name" className="w-[190px] capitalize"></input>
-</div>
-<div className="flex mt-4 justify-center">
-<label htmlFor="gender" className="w-[60px] mr-12">Gender:</label>
-<select type="input" name="gender" id="gender" className="w-[190px] capitalize">
-    {formData.genders.map(gender => <option key={gender} value={gender}>{gender}</option>)}
-</select>
-</div>
-<div className="flex mt-4 justify-center">
-<label htmlFor="brand" className="w-[60px] mr-12">Brand:</label>
-<select type="input" name="brand" id="brand" className="w-[190px] capitalize">
-{formData.brands.map(brand => <option key={brand} value={brand}>{brand}</option>)}
-
-</select>
-</div>
-<div className="flex mt-4 justify-center">
-<label htmlFor="sizes" className="w-[60px] mr-12">Sizes and Quantities:</label>
-<textarea 
-type="input" name="sizes" 
-placeholder={type=== 'clothing' 
-? 'ex: XL : 5, M : 15,  S : 13 You should add comma as shown in example' 
-: 'ex: 43 : 10, 46 : 15, 42 : 13 You should add comma as shown in example'} 
-id="sizes" className="w-[190px]">
-</textarea>
-</div>
-<div className="flex mt-4 justify-center">
-<label htmlFor="imageUrl" className="w-[60px] mr-12">ImageUrl:</label>
-<input type="input" name="imageUrl" id="imageUrl" className="w-[190px]" />
-</div>
-<div className="flex mt-4 justify-center">
-<label htmlFor="color"  className="w-[60px] mr-12">Color:</label>
-<input type="input" name="color" id="color" className="w-[190px]" />
-</div>
-<div className="flex mt-4 justify-center">
-<label htmlFor="price"  className="w-[60px] mr-12">Price:</label>
-<input type="number" name="price" id="price" className="w-[190px]" />
-</div>
-<div className="flex mt-4 justify-center">
-<button type="submit" className="py-2 px-10 mb-2 rounded-md text-white bg-[#d9b99b] font-bold">Submit</button>
-</div> */}
