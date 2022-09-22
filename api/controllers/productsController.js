0a049b583ed23c0c00c0973514e8474400b0ad58
vladislav99dev <vladislavcho99@abv.bg>
2022-09-22 11:28:44 +0300

    add new Error handling and new names to ProductsController

--- api/controllers/productsController.js
+++ api/controllers/productsController.js
@@ -11,32 +11,29 @@ const getManyHandler = async(req,res) => {
             throw {status:400, message:'No such gender found in database!'}
         }
         if(gender === 'all') {
-            const dbResponse = await productsServices.findAll();
-            return res.status(200).json(dbResponse);
+            const allProducts = await productsServices.findAll();
+            return res.status(200).json(allProducts);
         }
-        const dbResponse = await productsServices.findByGender(gender);
-        res.status(200).json(dbResponse);
+        const products = await productsServices.findByGender(gender);
+        res.status(200).json(products);
     }catch(err){
-        res.status(err.status).json({message:err.message})
+        if(err.status) return res.status(err.status).json({message:err.message});
+        console.log(err)
     }
 };
-
-
 
 const getOneHandler = async(req,res) => {
     console.log(`GET ${req.originalUrl}`);
     const {productId} = req.params
     try{
-        const dbResponse = await productsServices.findById(productId)
-        res.status(200).json(dbResponse)
+        const product = await productsServices.findById(productId);
+        if(!product) throw {status:400, message:'There is no product with this id!'};
+        res.status(200).json(product)
     } catch(err){
-    res.status(404).json({ message: "Product with this id was not found!" });
+        if(err.status) return res.status(err.status).jsson({message:err.message})
+        console.log(err)
     }
-    res.end();
 }
-
-
-
 
 
 router.get(["/men","/women","/girls","/boys","/all"], getManyHandler)
