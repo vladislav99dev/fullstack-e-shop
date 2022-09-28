7aa1b8d5226ae833628566e6e4012b4379d5f5fc
vladislav99dev <vladislavcho99@abv.bg>
2022-09-28 15:20:11 +0300

    accept user vie props and delete recently used context

--- client/src/components/NavBar/UserLinks.js
+++ client/src/components/NavBar/UserLinks.js
@@ -1,33 +1,30 @@
 import { Link } from "react-router-dom";
 
-
-import { useAuthContext } from "../../context/AuthContext"
-
 const UserLinks = ({
-    clickHandler
+    clickHandler,
+    user
 }) => {
-    const {user} = useAuthContext();
 
     return(
         <>
-        {!user.email
-        ?<>
-            <div className="container-links">
-                <Link to={"users/login"} className="mobile-links" onClick={clickHandler} >Login</Link>
-            </div>
-            <div className="container-links border-0">
-                <Link to={"users/register"} className="mobile-links " onClick={clickHandler}>Register</Link>
-            </div>
-        </>
-        :<>
-            <div className="container-links">
-                <Link to={"users/logout"} className="mobile-links" onClick={clickHandler}>Logout</Link>
-            </div>
-            <div className="container-links border-0">
-                <Link to={"#"} className="mobile-links" onClick={clickHandler}>Edit Profile</Link>
-            </div>
-        </>
-        }
+            {!user.email
+            ?<>
+                <div className="container-links">
+                    <Link to={"users/login"} className="mobile-links" onClick={clickHandler} >Login</Link>
+                </div>
+                <div className="container-links border-0">
+                    <Link to={"users/register"} className="mobile-links " onClick={clickHandler}>Register</Link>
+                </div>
+            </>
+            :<>
+                <div className="container-links">
+                    <Link to={"users/logout"} className="mobile-links" onClick={clickHandler}>Logout</Link>
+                </div>
+                <div className="container-links border-0">
+                    <Link to={"#"} className="mobile-links" onClick={clickHandler}>Edit Profile</Link>
+                </div>
+            </>
+            }
         </>
     )
 }
