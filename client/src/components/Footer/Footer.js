1824cd05ece4a7eb2cc0da0d92a5dce90bce1fe6
vladislav99dev <vladislavcho99@abv.bg>
2022-10-02 21:45:30 +0300

    add hover effects to Footer Elements

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -45,14 +45,14 @@ const Footer = () => {
         dispatch({type:type})
     }
     return (
-        <div className="bg-black md:grid md:grid-cols-4">
+        <div className="bg-black md:grid md:grid-cols-5 mt-[20rem] w-full">
             <div>
-                <ul className="text-white px-6 pt-8 pb-4 font-semibold">
-                    <li className="py-[0.4rem] text-sm">FIND A STORE</li>
-                    <li className="py-[0.4rem] text-sm">NIKE JOURNAL</li>
-                    <li className="py-[0.4rem] text-sm">BECOME A MEMBER</li>
-                    <li className="py-[0.4rem] text-sm">FEEDBACK</li>
-                    <li className="py-[0.4rem] text-sm">PROMO CODES</li>
+                <ul className="text-white px-6 pt-8 pb-4 font-semibold w-full lg:ml-20">
+                    <li className="py-[0.4rem] text-sm hover:cursor-pointer hover:text-[#00df9a]">FIND A STORE</li>
+                    <li className="py-[0.4rem] text-sm hover:cursor-pointer hover:text-[#00df9a]">NIKE JOURNAL</li>
+                    <li className="py-[0.4rem] text-sm hover:cursor-pointer hover:text-[#00df9a]">BECOME A MEMBER</li>
+                    <li className="py-[0.4rem] text-sm hover:cursor-pointer hover:text-[#00df9a]">FEEDBACK</li>
+                    <li className="py-[0.4rem] text-sm hover:cursor-pointer hover:text-[#00df9a]">PROMO CODES</li>
                 </ul>
             </div>
 
@@ -91,93 +91,32 @@ const Footer = () => {
         <DesktopCard 
         titleName={"GET HELP"}
         services={["Order Status","Shipping and Delivery","Returns", "Payment Options", "Contact us"]}
+        
         />
-
-
-
-
-        {/* <div onClick={toggler.bind(null,"getHelp")} className='flex justify-between cursor-pointer bg-[#00df9a] hover:bg-green-300 ease-in-out duration-200 md:hidden'>
-            <ul  className="text-white px-6 py-4 font-semibold text-sm">GET HELP
-                    {togglesState.getHelp 
-                    ?<>
-                        <li className="py-[0.25rem] text-sm mt-4 text-green-100">Order Status</li>
-                        <li className="py-[0.25rem] text-sm text-green-100">Shipping and Delivery</li>
-                        <li className="py-[0.25rem] text-sm text-green-100">Reteurns</li>
-                        <li className="py-[0.25rem] text-sm text-green-100">Payment Options</li>
-                        <li className="py-[0.25rem] text-sm text-green-100">Contact us</li> 
-                    </>
-                    :null
-                    }
-            </ul>
-            {togglesState.getHelp 
-            ? <BiMinus className='mt-4 mr-4' color={'white'} size={30}/> 
-            : <BiPlus className='mt-4 mr-4' color={'white'} size={30}/>
-            }   
-        </div> */}
-
-        {/* <div onClick={toggler.bind(null,"about")} className='flex justify-between cursor-pointer bg-[#00df9a] hover:bg-green-300 ease-in-out duration-200 md:hidden'>
-            <ul  className="text-white px-6 py-4 font-semibold text-sm">ABOUT NIKE
-                    {togglesState.about 
-                    ?<>
-                        <li className="py-[0.25rem] text-sm mt-4 text-green-100">News</li>
-                        <li className="py-[0.25rem] text-sm text-green-100">Careers</li>
-                        <li className="py-[0.25rem] text-sm text-green-100">Reteurns</li>
-                        <li className="py-[0.25rem] text-sm text-green-100">Investors</li>
-                        <li className="py-[0.25rem] text-sm text-green-100">Sustainability</li> 
-                    </>
-                    :null
-                    }
-            </ul>
-            {togglesState.about 
-            ? <BiMinus className='mt-4 mr-4' color={'white'} size={30}/> 
-            : <BiPlus className='mt-4 mr-4' color={'white'} size={30}/>
-            }   
-        </div>
-
-        <div onClick={toggler.bind(null,"apps")} className='flex justify-between cursor-pointer bg-[#00df9a] hover:bg-green-300 ease-in-out duration-200 md:hidden'>
-            <ul  className="text-white px-6 py-4 font-semibold text-sm">NIKE APPS
-                    {togglesState.apps 
-                    ?<>
-                        <li className="py-[0.25rem] text-sm mt-4 text-green-100">Nike-App</li>
-                        <li className="py-[0.25rem] text-sm text-green-100">Nike Run Club</li>
-                        <li className="py-[0.25rem] text-sm text-green-100">Nike Training Club</li>
-                        <li className="py-[0.25rem] text-sm text-green-100">SNKRS</li>
-                    </>
-                    :null
-                    }
-            </ul>
-            {togglesState.apps 
-            ? <BiMinus className='mt-4 mr-4' color={'white'} size={30}/> 
-            : <BiPlus className='mt-4 mr-4' color={'white'} size={30}/>
-            }   
-        </div> */}
-
-
-
-
-
-        <div className='flex px-6 py-4'>
-                <SiTwitter color={"#00df9a"} size={30}/>
+        <div className='flex px-6 py-4 mt-2 justify-center md:justify-end mr-4 '>
+            <div className='flex h-10 lg:mr-20'>
+                <SiTwitter color={"#00df9a"} size={30} className='ml-4 hover:cursor-pointer'/>
                 <SiFacebook color={"#00df9a"} className='ml-4 hover:cursor-pointer' size={30}/>
-                <SiInstagram color={"#00df9a"} className='ml-4' size={30}/>
-                <SiYoutube color={"#00df9a"} className='ml-4' size={30}/>
+                <SiInstagram color={"#00df9a"} className='ml-4 hover:cursor-pointer' size={30}/>
+                <SiYoutube color={"#00df9a"} className='ml-4 hover:cursor-pointer' size={30}/>
+            </div>
         </div>
 
-        <div className='md:flex'>
+        <div className='flex -ml-2 md:ml-[10rem] col-span-2 '>
             <div className='flex py-4 px-6'>
                 <MdLocationOn color={"#00df9a"} size={20}/>
                 <p className='text-white text-sm ml-2'>Bulgaria</p>
             </div>
-            <p className='text-white text-sm px-6'>&copy; 2022 SUPREME FASHION SHOP, Inc.All Rights Reserved</p>
+            <p className='text-white text-sm px-6 py-4'>&copy; 2022 SUPREME FASHION SHOP, Inc.All Rights Reserved</p>
         </div>
 
-        <div>
+        <div className='col-span-3 flex justify-center'>
             <ul className='text-gray-400 px-6 py-4 text-sm md:flex'>
-                <li className='py-2 hover:text-white cursor-pointer'>Terms of Use</li>
-                <li className='py-2 hover:text-white cursor-pointer'>Terms of Sale</li>
-                <li className='py-2 hover:text-white cursor-pointer'>Company Details</li>
-                <li className='py-2 hover:text-white cursor-pointer'>Privacy &#38; Cookie Policy</li>
-                <li className='py-2 hover:text-white cursor-pointer'>Cookie Settings</li>
+                <li className='py-2 px-6 hover:text-white cursor-pointer'>Terms of Use</li>
+                <li className='py-2 px-6 hover:text-white cursor-pointer'>Terms of Sale</li>
+                <li className='py-2 px-6 hover:text-white cursor-pointer'>Company Details</li>
+                <li className='py-2 px-6 hover:text-white cursor-pointer'>Privacy &#38; Cookie Policy</li>
+                <li className='py-2 px-6 hover:text-white cursor-pointer'>Cookie Settings</li>
             </ul>
         </div>
 
