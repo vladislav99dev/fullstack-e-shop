3d1c761406a4ec503543eb32c38208060795aef1
vladislav99dev <vladislavcho99@abv.bg>
2022-10-05 17:26:15 +0300

    add services in tokenServices for both admin and user

--- api/services/tokenServices.js
+++ api/services/tokenServices.js
@@ -1,16 +1,24 @@
 const jwt = require('jsonwebtoken');
 
- const verify = (token) => {
-    return jwt.verify(token,process.env.ACCESS_TOKEN_SECRET);
+ const verifyAdmin = (token) => {
+    return jwt.verify(token,process.env.ADMIN_ACCESS_TOKEN_SECRET);
  }
+ const verifyUser = (token) => {
+   return jwt.verify(token,process.env.USER_ACCESS_TOKEN_SECRET);
+}
  
- const generate = (user) => {
-   return jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1d'});
+ const generateAdmin = (user) => {
+   return jwt.sign(user,process.env.ADMIN_ACCESS_TOKEN_SECRET,{expiresIn: '1d'});
+ }
+ const generateUser = (user) => {
+   return jwt.sign(user,process.env.USER_ACCESS_TOKEN_SECRET,{expiresIn: '1d'});
  }
 
  const tokenServices = {
-    verify,
-    generate
+   verifyAdmin,
+   verifyUser,
+   generateAdmin,
+   generateUser
  }
 
  module.exports = tokenServices;
