f5e6124c0a1b5f42de88851b141fb5c88dd04cc7
vladislav99dev <vladislavcho99@abv.bg>
2022-10-05 16:33:04 +0300

    make Edit profile work

--- client/src/components/PersonalInfo/EditProfile.js
+++ client/src/components/PersonalInfo/EditProfile.js
@@ -1,8 +1,16 @@
 import { useReducer,useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useModalsContext } from "../../context/ModalsContext";
+
 
 import { validateUserEdit } from "../../validations/userValidations";
+import * as userRequester from "../../services/userRequester";
 
-import Input from "./Input"
+
+import Input from "./Input";
+import AttentionModal from "../Modals/AttentionModal";
+import SuccessModal from "../Modals/SuccessModal";
+
 import ValidationMessage from "../ValidationMessage/validationMessage";
 
 
@@ -20,9 +28,8 @@ const reducerFormDataState = (state,{type,payload}) => {
     }
 }
 
-const EditProfile = ({user}) => {
+const EditProfile = ({user,login}) => {
     const [validationMessages,setValidationMessages] = useState([]);
-
     const [formDataState,dispatch] = useReducer(reducerFormDataState, {
         firstName: user.firstName,
         lastName: user.lastName,
@@ -31,27 +38,65 @@ const EditProfile = ({user}) => {
         state: user.state,
         city: user.city,
         zipCode: user.zipCode,
-        country: user.country,
         street: user.street,
         unitNumber: user.unitNumber,
         phoneNumber: user.phoneNumber,
     })
+    const navigate = useNavigate();
+    const {modalState,setFailedModal,setSuccessModal,resetModals} = useModalsContext();
 
     const changeFormData = (type,event) => {
         if(type === 'cleanup') return {};
         dispatch({type:type,payload:event.target.value})
     }
 
-    const submitHandler = (event) => {
+    const submitHandler = async(event) => {
         event.preventDefault();
         const validationsResponse = validateUserEdit(formDataState);
         if(validationsResponse.length > 0) return setValidationMessages(validationsResponse);
+
+        try{
+            const response = await userRequester.edit(formDataState,user._id);
+            const jsonResponse = await response.json();
+            if(response.status !== 200) throw {message:jsonResponse.message};
+            if(response.status === 200) {
+                login(jsonResponse.user)
+                return setSuccessModal(jsonResponse.message)
+            } 
+        }catch(err){
+            console.log(err);
+            return setFailedModal(err.message)
+        }
+        
+    }
+
+    const modalBtnHandlers = (modal) => {
+        resetModals()
+        if(modal === 'success')  return navigate('/home')
     }
 
     return (
         <>
         <h1 className="text-[#00df9a] text-2xl italic uppercase font-bold w-full text-center">Edit Profile</h1>
         <form onSubmit={submitHandler} className="mt-2">
+            {modalState.isFailed.value 
+            ? <AttentionModal
+                titleMessage={"Something went wrong"}
+                descriptionMessage={modalState.isFailed.message}
+                buttonName={"Try again"}
+                buttonHandler={resetModals}
+            />
+            :null
+            }
+            {modalState.isSuccess.value 
+            ? <SuccessModal
+                titleMessage={"Success!"}
+                descriptionMessage={modalState.isFailed.message}
+                buttonName={"Go to home page"}
+                buttonHandler={modalBtnHandlers.bind(null,"success")}
+            />
+            :null
+            }
             {validationMessages.length > 0 
             ?   validationMessages.map((x) => <ValidationMessage key={x} message={x}/>)
             :   null
