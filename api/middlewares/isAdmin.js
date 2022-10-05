42409058fbf54ecd39d0777a6f769d2be49eef64
vladislav99dev <vladislavcho99@abv.bg>
2022-10-05 17:26:33 +0300

    change isAdmin middleware with new functiions

--- api/middlewares/isAdmin.js
+++ api/middlewares/isAdmin.js
@@ -5,10 +5,11 @@ const isAdmin = (req, res, next) => {
     const token = req.headers.authorization;
     if(token === 'undefined' || !token) return res.status(401).json({isAdmin:false, message: 'Access token is not provided!'})
     try{
-        const admin = tokenServices.verify(token);
+        const admin = tokenServices.verifyAdmin(token);
     } catch(err){
         const[error,errorMessage] = Object.values(err)
         if(errorMessage === 'invalid token') return res.status(401).json({isAdmin:false,message:'Access token is invalid!'});
+        if(errorMessage === 'jwt malformed') return  res.status(401).json({isAdmin:false,message:'Access token is invalid!'});
         if(errorMessage === 'jwt expired') return  res.status(401).json({isAdmin:false,message:'Access token expired,you should re-login!'});
     }
     next()
