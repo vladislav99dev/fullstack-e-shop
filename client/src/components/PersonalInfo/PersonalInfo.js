972371239db623d6818b6a213d7b88e0c49d847b
vladislav99dev <vladislavcho99@abv.bg>
2022-10-05 17:24:49 +0300

    cahnge some border colors

--- client/src/components/PersonalInfo/PersonalInfo.js
+++ client/src/components/PersonalInfo/PersonalInfo.js
@@ -43,10 +43,10 @@ const PersonalInfo = () => {
     }
     return (
         <div className="flex justify-center">
-            <div className="w-[15%] border-4 border-[#00df9a]">
-                <button onClick={btnClickHandler.bind(null,"edit")} className="py-4 border-b-4 border-[#00df9a] w-full text-left font-extrabold text-[#28282B]">Edit Profile</button>
-                <button onClick={btnClickHandler.bind(null,"changePassword")} className="py-4 border-b-4 border-[#00df9a] w-full text-left font-extrabold text-[#28282B]">Change Password</button>
-                <button onClick={btnClickHandler.bind(null,"orders")} className="py-4 border-b-4 border-[#00df9a] w-full text-left font-extrabold text-[#28282B]">Orders</button>
+            <div className="w-[15%] border-4 border-[#28282B]">
+                <button onClick={btnClickHandler.bind(null,"edit")} className="py-4 border-b-4 border-[#28282B] w-full text-left font-extrabold text-[#28282B]">Edit Profile</button>
+                <button onClick={btnClickHandler.bind(null,"changePassword")} className="py-4 border-b-4 border-[#28282B] w-full text-left font-extrabold text-[#28282B]">Change Password</button>
+                <button onClick={btnClickHandler.bind(null,"orders")} className="py-4 border-b-4 border-[#28282B] w-full text-left font-extrabold text-[#28282B]">Orders</button>
             </div>
             <div className="w-[40%] border-2 border-[#00df9a] border-l-0">
                 { buttonsState.edit ? <EditProfile user={user} login={login}/> : null }
