80e4a3588fb1ee490f482b9288d3381924820c83
vladislav99dev <vladislavcho99@abv.bg>
2022-10-05 13:01:49 +0300

    create custom Input Element

--- client/src/components/PersonalInfo/Input.js
+++ client/src/components/PersonalInfo/Input.js
@@ -1,6 +1,7 @@
 const Input = ({
     labelName,
-    defaultValue
+    defaultValue,
+    changeFormData
     }) => {
     return (
         <>
@@ -8,7 +9,7 @@ const Input = ({
         < label className="w-[35%] text-center z-10 rounded-3xl bg-green-200 ">{labelName}</label>
     </div>
     <div className="flex justify-center -mt-2">
-        <input className="w-[50%] border-2 border-[#00df9a] py-[0.25rem] rounded-lg hover:border-green-200 text-center "defaultValue={defaultValue} />
+        <input onBlur={changeFormData} className="w-[50%] border-2 border-[#00df9a] py-[0.25rem] rounded-lg hover:border-green-200 text-center" defaultValue={defaultValue} />
     </div>
         </>
 
