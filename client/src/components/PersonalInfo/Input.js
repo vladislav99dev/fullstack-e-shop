0f5a65335222e4353e161d92a4759202c78785ef
vladislav99dev <vladislavcho99@abv.bg>
2022-10-08 10:19:23 +0300

    add Input dynamic type

--- client/src/components/PersonalInfo/Input.js
+++ client/src/components/PersonalInfo/Input.js
@@ -1,7 +1,8 @@
 const Input = ({
     labelName,
     defaultValue,
-    changeState
+    changeState,
+    type
     }) => {
     return (
         <>
@@ -9,7 +10,7 @@ const Input = ({
         < label className="w-[35%] text-center z-10 rounded-3xl bg-green-200 ">{labelName}</label>
     </div>
     <div className="flex justify-center -mt-2">
-        <input onBlur={changeState} className="w-[50%] border-2 border-[#00df9a] py-[0.25rem] rounded-lg hover:border-green-200 text-center" defaultValue={defaultValue ? defaultValue : ''} />
+        <input onBlur={changeState} type={type ? type : 'text'} className="w-[50%] border-2 border-[#00df9a] py-[0.25rem] rounded-lg hover:border-green-200 text-center" defaultValue={defaultValue ? defaultValue : ''} />
     </div>
         </>
 
