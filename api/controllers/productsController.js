a030c1edfaa81c5854f481d82c8b2883c10639dd
vladislav99dev <vladislavcho99@abv.bg>
2022-10-09 22:24:15 +0300

    refactor productsContrroller

--- api/controllers/productsController.js
+++ api/controllers/productsController.js
@@ -1,77 +1,73 @@
 const router = require('express').Router();
 
-const productsServices = require('../services/productsServices')
+
+const findAllProducts = require("../utils/products/findAllProducts");
+const findByGender = require("../utils/products/findByGender");
+const findAndFilterProducts = require("../utils/products/findAndFilterProducts");
+const findProductById = require("../utils/products/findProductById");
+const extractEmptyPropsAndSizesFromFilter = require("../utils/products/extractEmptyPropsAndSizesFromFilter");
+const filterProductsBySize = require("../utils/products/filterProductsBySize");
 
 
 const getManyHandler = async(req,res) => {
     console.log(`GET ${req.originalUrl}`);
+
     const gender = req.path.split('/')[1];
+
+    let products = [];
+
+    if(gender !== 'men' 
+    && gender !== 'women' 
+    && gender !=='boys' 
+    && gender !== 'girls' 
+    && gender !== 'all') return res.status(400).json({message:"No such gender found in database!"})
+
     try{
-        if(gender !== 'men' && gender !== 'women' && gender !=='boys' && gender !== 'girls' && gender !== 'all'){
-            throw {status:400, message:'No such gender found in database!'}
-        }
-        if(gender === 'all') {
-            const allProducts = await productsServices.findAll();
-            return res.status(200).json(allProducts);
-        }
-        const products = await productsServices.findByGender(gender);
+        if(gender === 'all') products = await findAllProducts();
+
+        if(gender !== 'all') products = await findByGender(gender);
+
         res.status(200).json(products);
     }catch(err){
         if(err.status) return res.status(err.status).json({message:err.message});
-        console.log(err)
     }
 };
 
 const getManyFiltered = async(req,res) => {
     console.log(`POST ${req.originalUrl}`);
+
     const data = req.body;
 
-    const extractEmptyPropertiesAndSizes = (data) => {
-        const sizes = [...data.sizes];
-        for (const prop in data) {
-            if(data[prop] === '' || Array.isArray(data[prop])) delete data[prop];
-        }
-        return [data,sizes]
-    }
-    const filterProductsBySizes = (products) => {
-        for (const product of products) {
-            for (let i = 0; i < sizes.length; i++) {
-                let currSize = sizes[i];
-                if(product.sizes[currSize] > 0){
-                    break;
-                }
-                if(i  === sizes.length - 1){
-                    products = products.filter(x => x !== product)
-                }
-            }
-        }
-        return products
-    }
-
-    let [modifiedData,sizes] = extractEmptyPropertiesAndSizes(data);
+    const {filterData,filterSizes} = extractEmptyPropsAndSizesFromFilter(data);
 
     try {
-        let products = await productsServices.findByFilter(modifiedData)
-        if(products.length  === 0) return res.status(200).json({products:products})
-        const sizeFilteredProducts = filterProductsBySizes(products);
-        if(sizeFilteredProducts.length  === 0) return res.status(200).json({products:sizeFilteredProducts})
-        return res.status(200).json(sizeFilteredProducts)
+        let products = await findAndFilterProducts(filterData);
+
+        if(products.length === 0 ) return res.status(200).json({products:products});
+
+        const sizeFilteredProducts = filterProductsBySize(products,filterSizes);
+
+        if(sizeFilteredProducts.length === 0) return res.status(200).json({products:sizeFilteredProducts})
+
+        return res.status(200).json({products:sizeFilteredProducts});
     } catch(err){
-        console.log(err);
+        if(err.status) return res.status(err.status).json({message:err.message});
     }
-    res.end();
 }
+
 
 const getOneHandler = async(req,res) => {
     console.log(`GET ${req.originalUrl}`);
+
     const {productId} = req.params
+
     try{
-        const product = await productsServices.findById(productId);
-        if(!product) throw {status:400, message:'There is no product with this id!'};
+        const product = await findProductById(productId);
+
         res.status(200).json(product)
+
     } catch(err){
         if(err.status) return res.status(err.status).jsson({message:err.message})
-        console.log(err)
     }
 }
 
