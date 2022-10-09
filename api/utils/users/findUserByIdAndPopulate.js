f22864d1f8a25fbd94fdacd17f9f9ad59e745130
vladislav99dev <vladislavcho99@abv.bg>
2022-10-09 17:36:21 +0300

    fix userPpopulation in userUtils

--- api/utils/users/findUserByIdAndPopulate.js
+++ api/utils/users/findUserByIdAndPopulate.js
@@ -3,7 +3,7 @@ const userServices = require("../../services/userServices");
 
 
 const findUserByIdAndPopulate = async(id) => {
-    const {password, ...populatedUser} = await userServices.findByIdAndPopulate(id);
+    const {password,...populatedUser} = await userServices.findByIdAndPopulate(id);
     if(!populatedUser) throw {status:400, message:'There is no user with this id!'};
     return populatedUser;
 }
