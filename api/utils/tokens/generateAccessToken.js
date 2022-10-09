6bec34facf967cfb2bed0dc56fa82cc05a7f7758
vladislav99dev <vladislavcho99@abv.bg>
2022-10-09 15:35:37 +0300

    add generateAccessToken in utils/tokens folder

--- api/utils/tokens/generateAccessToken.js
+++ api/utils/tokens/generateAccessToken.js
@@ -1,16 +1,19 @@
-const jwt = require('jsonwebtoken');
-
+const jwt = require("jsonwebtoken");
 
 const admin = (user) => {
-    return jwt.sign(user,process.env.ADMIN_ACCESS_TOKEN_SECRET,{expiresIn: '1d'});
-  }
-  const user = (user) => {
-    return jwt.sign(user,process.env.USER_ACCESS_TOKEN_SECRET,{expiresIn: '1d'});
-  }
+  return jwt.sign(user, process.env.ADMIN_ACCESS_TOKEN_SECRET, {
+    expiresIn: "1d",
+  });
+};
+const user = (user) => {
+  return jwt.sign(user, process.env.USER_ACCESS_TOKEN_SECRET, {
+    expiresIn: "1d",
+  });
+};
 
-  const generateAccessToken = {
-    admin,
-    user
-  }
+const generateAccessToken = {
+  admin,
+  user,
+};
 
-  module.exports = generateAccessToken ;
+module.exports = generateAccessToken;
