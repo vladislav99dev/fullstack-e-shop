7ff80cdd7fce1ca07b502df39e370f3c44abee9a
vladislav99dev <vladislavcho99@abv.bg>
2022-10-09 15:35:22 +0300

    add verifyAccessToken in utils/tokens folder

--- api/utils/tokens/verifyAccessToken.js
+++ api/utils/tokens/verifyAccessToken.js
@@ -1,15 +1,21 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const deleteUserAccessToken = require("./deleteUserAccessToken");
+
+const verifyAccessToken = async(user,token) => {
+  try{
+    if(user.isAdmin)  jwt.verify(token, process.env.ADMIN_ACCESS_TOKEN_SECRET);
+    if(!user.isAdmin)  jwt.verify(token, process.env.USER_ACCESS_TOKEN_SECRET);
+  } catch(err) {
+    const [error,errorMessage] = Object.values(err);
+
+    if(errorMessage === 'invalid token' || errorMessage ==='jwt malformed') throw {status:401,message:'Access token is invalid!'}
+
+    if(errorMessage === 'jwt expired') {
+      await deleteUserAccessToken(user._id);
+      throw  {status:401,message:'Access token expired,you should re-login!'}
+    }
+  }
+};
 
 
-const admin = (token) => {
-    return jwt.verify(token,process.env.ADMIN_ACCESS_TOKEN_SECRET);
- }
- const user = (token) => {
-   return jwt.verify(token,process.env.USER_ACCESS_TOKEN_SECRET);
-}
-
-const verifyAccessToken = {
-    admin,
-    user
-}
 module.exports = verifyAccessToken;
