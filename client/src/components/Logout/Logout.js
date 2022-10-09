412a37c565b98f67e42700d7c56751fca2dd1a9c
vladislav99dev <vladislavcho99@abv.bg>
2022-10-09 17:36:01 +0300

    call userREquester in Logout

--- client/src/components/Logout/Logout.js
+++ client/src/components/Logout/Logout.js
@@ -10,12 +10,12 @@ import AttentionModal from "../Modals/AttentionModal";
 
 
 const Logout = () => {
-    const {logout} = useAuthContext();
+    const {user,logout} = useAuthContext();
     const navigate = useNavigate()
 
     const logoutHandler = async() => {
         try {
-            await userRequester.logout()
+            await userRequester.logout(null,user._id)
             logout()
             return navigate('/home')
         } catch(err){
