8ec706139734f09314d494b4ec3316d42bbeefb3
vladislav99dev <vladislavcho99@abv.bg>
2022-10-11 21:18:19 +0300

    change return value in verifyAccessToken function

--- api/utils/tokens/verifyAccessToken.js
+++ api/utils/tokens/verifyAccessToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
-const deleteUserAccessToken = require("./deleteUserAccessToken");
 
 const verifyAccessToken = async(user,token) => {
+  let isExpired ;
   try{
     if(user.isAdmin)  jwt.verify(token, process.env.ADMIN_ACCESS_TOKEN_SECRET);
     if(!user.isAdmin)  jwt.verify(token, process.env.USER_ACCESS_TOKEN_SECRET);
@@ -11,10 +11,12 @@ const verifyAccessToken = async(user,token) => {
     if(errorMessage === 'invalid token' || errorMessage ==='jwt malformed') throw {status:401,message:'Access token is invalid!'}
 
     if(errorMessage === 'jwt expired') {
-      await deleteUserAccessToken(user._id);
-      throw  {status:401,message:'Access token expired,you should re-login!'}
+      isExpired = true;
+      return isExpired
     }
   }
+  isExpired = false
+  return isExpired
 };
 
 
