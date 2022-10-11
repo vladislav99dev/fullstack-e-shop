e0358b9a7f47501c1fa3d86b3330ab63a4a43bc4
vladislav99dev <vladislavcho99@abv.bg>
2022-10-11 21:20:43 +0300

    remove required validation of Order Model

--- api/models/Order.js
+++ api/models/Order.js
@@ -3,17 +3,14 @@ const mongoose = require("mongoose");
 const orderSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    required: [true, "First name is required"],
     validate: [/^[a-z-']+$/i, "First name is not in valid format"],
   },
   lastName: {
     type: String,
-    required: [true, "Last Name is required"],
     validate: [/^[a-z-']+$/i, "Last name is not in valid format"],
   },
   email: {
     type: String,
-    required: [true, "Email is required"],
     validate: [
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
       "Email is not in valid format",
@@ -21,35 +18,28 @@ const orderSchema = new mongoose.Schema({
   },
   country: {
     type: String,
-    required: [true, "Country is required"],
     validate: [/^[a-z -']+$/i, "Country is not in valid format"],
   },
   city: {
     type: String,
-    required: [true, "City is required"],
     validate: [/^[a-z -']+$/i, "City is not in valid format"],
   },
   street: {
     type: String,
-    required: [true, "Street is required"],
     validate: [/^[a-z -'0-9]+$/i, "Street is not in valid format"],
   },
   state: {
     type: String,
-    required: [true, "State is required"],
     validate: [/[A-Za-z]+$/i, "Street is not in valid format"],
   },
   zipCode: {
     type: String,
-    required: [true, "Zip Code is required"],
   },
   unitNumber: {
     type: String,
-    required: [true, "Unit number is required"]
   },
   phoneNumber: {
     type: String,
-    required: [true, "Phone number is required"],
     validate: [/[0-9]+$/i, "Phone number is not in valid format"],
   },
   productsOrdered: [
