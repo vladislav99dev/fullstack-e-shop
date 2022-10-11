7cff380385df68e6754ebbc0a401910a7315c45b
vladislav99dev <vladislavcho99@abv.bg>
2022-10-11 21:11:21 +0300

    fix err message in checkUserPassword

--- api/utils/users/checkUserPassword.js
+++ api/utils/users/checkUserPassword.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcrypt");
 
 const checkUserPassword = async(submitedPassword,userPassword) => {
     const isValid = await bcrypt.compare(submitedPassword,userPassword);
-    if(!isValid) throw {status:401, message:'Incorrect password'};
+    if(!isValid) throw {status:401, message:'Incorrect Email or Password'};
 }
 
 module.exports = checkUserPassword
