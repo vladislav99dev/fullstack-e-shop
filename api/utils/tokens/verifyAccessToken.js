1dff7e1a14d93dd12a4514308ede969c510c7a0c
vladislav99dev <vladislavcho99@abv.bg>
2022-10-12 12:22:40 +0300

    fix name of return value in verifyAccessToken

--- api/utils/tokens/verifyAccessToken.js
+++ api/utils/tokens/verifyAccessToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const verifyAccessToken = async(user,token) => {
-  let isExpired ;
+  let isVerified ;
   try{
     if(user.isAdmin)  jwt.verify(token, process.env.ADMIN_ACCESS_TOKEN_SECRET);
     if(!user.isAdmin)  jwt.verify(token, process.env.USER_ACCESS_TOKEN_SECRET);
@@ -11,12 +11,12 @@ const verifyAccessToken = async(user,token) => {
     if(errorMessage === 'invalid token' || errorMessage ==='jwt malformed') throw {status:401,message:'Access token is invalid!'}
 
     if(errorMessage === 'jwt expired') {
-      isExpired = true;
-      return isExpired
+      isVerified = false;
+      return isVerified
     }
   }
-  isExpired = false
-  return isExpired
+  isVerified = true
+  return isVerified
 };
 
 
