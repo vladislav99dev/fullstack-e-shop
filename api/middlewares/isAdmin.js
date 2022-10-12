1d100a9698ca39ef9518310ff7be9865dfb3162c
vladislav99dev <vladislavcho99@abv.bg>
2022-10-12 12:23:58 +0300

    fix isAdmin middleware for me secure admin token check

--- api/middlewares/isAdmin.js
+++ api/middlewares/isAdmin.js
@@ -1,15 +1,30 @@
 const verifyAccessToken = require('../utils/tokens/verifyAccessToken');
+const tokenServices = require("../services/token/tokenServices");
+const userServices = require("../services/user/userServices");
 
-const isAdmin = (req, res, next) => {
+const isAdmin = async(req, res, next) => {
     const token = req.headers.authorization;
-    if(token === 'undefined' || !token) return res.status(401).json({isAdmin:false, message: 'Access token is not provided!'})
+    const {profileId} = req.body;
+
+    if(!token) return res.status(401).json({isAdmin:false, message: 'Access token is not provided!'})
+    
     try{
-        const admin = verifyAccessToken.admin(token);
+        const user = await userServices.findById(profileId);
+
+        const tokenDocument = await tokenServices.findByUserId(profileId)
+
+        if(tokenDocument.token !== token) throw {status:400,message:"Invalid accessToken!"};
+
+        const isVerified = await verifyAccessToken(user,token);
+
+        if(!isVerified) {
+            await tokenServices.deleteByUserId(profileId);
+            return res.status(401).json({message:"Access Token expired please re-login"});
+        }
+
     } catch(err){
-        const[error,errorMessage] = Object.values(err)
-        if(errorMessage === 'invalid token') return res.status(401).json({isAdmin:false,message:'Access token is invalid!'});
-        if(errorMessage === 'jwt malformed') return  res.status(401).json({isAdmin:false,message:'Access token is invalid!'});
-        if(errorMessage === 'jwt expired') return  res.status(401).json({isAdmin:false,message:'Access token expired,you should re-login!'});
+        if(err.path === '_id') return res.status(400).json({message:'Profile id is invalid!'});
+        if(err.status) return res.status(err.status).json({message:err.message});
     }
     next()
 }
