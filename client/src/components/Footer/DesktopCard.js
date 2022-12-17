6c47b7c38fb52c5bcbbf0a37c1dc1eaedfe1be54
vladislav99dev <vladislavcho99@abv.bg>
2022-12-18 01:03:16 +0200

    add keys when maping in footer

--- client/src/components/Footer/DesktopCard.js
+++ client/src/components/Footer/DesktopCard.js
@@ -2,10 +2,9 @@ const DesktopCard = ({
     titleName,
     services
 }) => {
-    console.log(services)
     return (
         <ul className="hidden md:block text-white px-6 py-4 mt-4 w-full hover:cursor-pointer hover:text-[#00df9a] lg:text-center">{titleName}
-            {services.map((x) => <li  className="text-gray-400 text-sm hover:cursor-pointer hover:text-green-200">{x}</li>
+            {services.map((x,index) => <li key={index} className="text-gray-400 text-sm hover:cursor-pointer hover:text-green-200">{x}</li>
             )}
         </ul>
     )
