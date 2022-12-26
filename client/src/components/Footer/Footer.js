d08876eaf1a16c777426cc025c03544007521914
vladislav99dev <vladislavcho99@abv.bg>
2022-12-26 17:37:29 +0200

    move out all css footer styling to module css

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,130 +1,128 @@
-import { useReducer } from 'react'
+import { useReducer } from "react";
 
-import {SiTwitter,SiFacebook,SiInstagram,SiYoutube} from 'react-icons/si'
-import {MdLocationOn} from "react-icons/md"
+import { SiGithub, SiFacebook, SiInstagram, SiLinkedin } from "react-icons/si";
+import { MdLocationOn } from "react-icons/md";
+import styles from "./Footer.module.css";
 
-import MobileToggle from './MobileToggle'
-import DesktopCard from './DesktopCard'
+import DesktopCard from "./DesktopCard";
 
 const initialTogglesState = {
-    hetHelp:false,
-    about:false,
-    apps:false,
-}
+  hetHelp: false,
+  about: false,
+  apps: false,
+};
 
-
-const reducerToggleState = (state,action) => {
-    switch (action.type) {
-        case "getHelp":
-            return {
-                getHelp:!state.getHelp,
-                about:false,
-                apps:false
-            }
-        case "about":
-            return {
-                getHelp:false,
-                about:!state.about,
-                apps:false
-            }  
-        case "apps":
-            return {
-                getHelp:false,
-                about:false,
-                apps:!state.apps
-            }   
-        default:
-            break;
-    }
-}
+const reducerToggleState = (state, action) => {
+  switch (action.type) {
+    case "getHelp":
+      return {
+        getHelp: !state.getHelp,
+        about: false,
+        apps: false,
+      };
+    case "about":
+      return {
+        getHelp: false,
+        about: !state.about,
+        apps: false,
+      };
+    case "apps":
+      return {
+        getHelp: false,
+        about: false,
+        apps: !state.apps,
+      };
+    default:
+      break;
+  }
+};
 
 const Footer = () => {
-    const [togglesState,dispatch] = useReducer(reducerToggleState,initialTogglesState);
+  const [togglesState, dispatch] = useReducer(
+    reducerToggleState,
+    initialTogglesState
+  );
 
-    const toggler = (type) => {
-        dispatch({type:type})
-    }
-    return (
-        <div className="bg-[#28282B] md:grid md:grid-cols-5 mt-[27rem] w-full h-full">
-            <div>
-                <ul className="text-white px-6 pt-8 pb-4 font-semibold w-full lg:ml-20">
-                    <li className="py-[0.4rem] text-sm hover:cursor-pointer hover:text-[#00df9a]">FIND A STORE</li>
-                    <li className="py-[0.4rem] text-sm hover:cursor-pointer hover:text-[#00df9a]">NIKE JOURNAL</li>
-                    <li className="py-[0.4rem] text-sm hover:cursor-pointer hover:text-[#00df9a]">BECOME A MEMBER</li>
-                    <li className="py-[0.4rem] text-sm hover:cursor-pointer hover:text-[#00df9a]">FEEDBACK</li>
-                    <li className="py-[0.4rem] text-sm hover:cursor-pointer hover:text-[#00df9a]">PROMO CODES</li>
-                </ul>
-            </div>
+  const toggler = (type) => {
+    dispatch({ type: type });
+  };
 
-        <MobileToggle 
-        togglesState={togglesState} 
-        clickHandler ={toggler.bind(null,"getHelp")} 
-        titleName={"GET HELP"} 
-        services={["Order Status","Shipping and Delivery","Returns", "Payment Options", "Contact us"]}
-        state={"getHelp"}
-        />
+  return (
+    <div className={styles.container}>
+      <div>
+        <ul className={styles.firstCol}>
+          <li>FIND A STORE</li>
+          <li>NIKE JOURNAL</li>
+          <li>BECOME A MEMBER</li>
+          <li>FEEDBACK</li>
+          <li>PROMO CODES</li>
+        </ul>
+      </div>
 
-        <MobileToggle 
-        togglesState={togglesState} 
-        clickHandler ={toggler.bind(null,"about")} 
-        titleName={"ABOUT NIKE"} 
-        services={["News","Carrers","Investors", "Sustainability"]}
-        state={"about"}
-        />
-
-        <MobileToggle 
-        togglesState={togglesState} 
-        clickHandler ={toggler.bind(null,"apps")} 
-        titleName={"NIKE APPS"} 
-        services={["Nike App","Nike Run Club","Nike Training Club", "SNKRS"]}
-        state={"apps"}
-        />
-
-        <DesktopCard 
+      <DesktopCard
         titleName={"GET HELP"}
-        services={["Order Status","Shipping and Delivery","Returns", "Payment Options", "Contact us"]}
-        />
-        <DesktopCard 
-        titleName={"GET HELP"}
-        services={["Order Status","Shipping and Delivery","Returns", "Payment Options", "Contact us"]}
-        />
-        <DesktopCard 
-        titleName={"GET HELP"}
-        services={["Order Status","Shipping and Delivery","Returns", "Payment Options", "Contact us"]}
-        
-        />
-        <div className='flex px-6 py-4 mt-2 justify-center md:justify-end mr-4 '>
-            <div className='flex h-10 lg:mr-20'>
-                <SiTwitter color={"#00df9a"} size={30} className='ml-4 hover:cursor-pointer'/>
-                <SiFacebook color={"#00df9a"} className='ml-4 hover:cursor-pointer' size={30}/>
-                <SiInstagram color={"#00df9a"} className='ml-4 hover:cursor-pointer' size={30}/>
-                <SiYoutube color={"#00df9a"} className='ml-4 hover:cursor-pointer' size={30}/>
-            </div>
+        services={[
+          "Order Status",
+          "Shipping and Delivery",
+          "Returns",
+          "Payment Options",
+          "Contact us",
+        ]}
+        state={togglesState.getHelp}
+        clickHandler={toggler.bind(null, "getHelp")}
+      />
+      <DesktopCard
+        titleName={"ABOUT NIKE"}
+        services={["News", "Carrers", "Investors", "Sustainability"]}
+        state={togglesState.about}
+        clickHandler={toggler.bind(null, "about")}
+      />
+      <DesktopCard
+        titleName={"NIKE APPS"}
+        services={["Nike App", "Nike Run Club", "Nike Training Club", "SNKRS"]}
+        state={togglesState.apps}
+        clickHandler={toggler.bind(null, "apps")}
+      />
+      <div className={styles.icons}>
+        <a href="https://github.com/vladislav99dev" target="_blank">
+          <SiGithub color={"#00df9a"} size={30} />
+        </a>
+        <a
+          href="https://www.facebook.com/profile.php?id=100058096826543"
+          target="_blank"
+        >
+          <SiFacebook color={"#00df9a"} size={30} />
+        </a>
+        <a href="https://www.instagram.com/vladislavdorovski2/" target="_blank">
+          <SiInstagram color={"#00df9a"} size={30} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/vladislav-dorovski-7bb854252/"
+          target="_blank"
+        >
+          <SiLinkedin color={"#00df9a"} size={30} />
+        </a>
+      </div>
+
+      <div className={styles["location-copyright"]}>
+        <div className="flex">
+          <MdLocationOn color={"#00df9a"} size={25} />
+          <p>Pleven, Bulgaria</p>
         </div>
+        <p>&copy; 2022 SUPREME FASHION SHOP, Inc.All Rights Reserved</p>
+      </div>
 
-        <div className='flex -ml-2 md:ml-[10rem] col-span-2 '>
-            <div className='flex py-4 px-6'>
-                <MdLocationOn color={"#00df9a"} size={20}/>
-                <p className='text-white text-sm ml-2'>Bulgaria</p>
-            </div>
-            <p className='text-white text-sm px-6 py-4'>&copy; 2022 SUPREME FASHION SHOP, Inc.All Rights Reserved</p>
-        </div>
-
-        <div className='col-span-3 flex justify-center'>
-            <ul className='text-gray-400 px-6 py-4 text-sm md:flex'>
-                <li className='py-2 px-6 hover:text-white cursor-pointer'>Terms of Use</li>
-                <li className='py-2 px-6 hover:text-white cursor-pointer'>Terms of Sale</li>
-                <li className='py-2 px-6 hover:text-white cursor-pointer'>Company Details</li>
-                <li className='py-2 px-6 hover:text-white cursor-pointer'>Privacy &#38; Cookie Policy</li>
-                <li className='py-2 px-6 hover:text-white cursor-pointer'>Cookie Settings</li>
-            </ul>
-        </div>
-
-
-        </div>   
-
-    )
-}
-
+      <div className={styles.terms}>
+        <ul>
+          <li>Terms of Use</li>
+          <li>Terms of Sale</li>
+          <li>Company Details</li>
+          <li>Privacy &#38; Cookie Policy</li>
+          <li>Cookie Settings</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
