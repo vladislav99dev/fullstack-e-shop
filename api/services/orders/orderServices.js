4d1103309d12c8e90afa60dd466cf7185057d933
vladislav99dev <vladislavcho99@abv.bg>
2023-01-09 21:26:16 +0200

    add more dbServices to ordersServices

--- api/services/orders/orderServices.js
+++ api/services/orders/orderServices.js
@@ -17,9 +17,22 @@ const findByUserId = async(id) => {
     return orders
 }
 
+const findById = async(id) => {
+    let order = await Order.findById(id);
+    if(!order) throw {status:404,message:'Product with this id was not found!'};
+    return order
+}
+
+const getAll = async(id) => {
+    let orders = await Order.find();
+    return orders
+}
+
 const orderServices = {
     create,
-    findByUserId
+    findByUserId,
+    findById,
+    getAll
 }
 
 module.exports = orderServices;
