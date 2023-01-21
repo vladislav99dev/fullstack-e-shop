f2e759f5e083cfa4cc15f9a7d0f684e814931758
vladislav99dev <vladislavcho99@abv.bg>
2023-01-21 20:41:22 +0200

    Separate code function ussed in admin controller into another file

--- api/controllers/adminController.js
+++ api/controllers/adminController.js
@@ -3,23 +3,23 @@ const router = require("express").Router();
 const productDataValidation = require("../validations/productDataValidation");
 const productServices = require("../services/products/productsServices");
 const orderServices = require("../services/orders/orderServices");
-
+const formatOrderData = require( "../utils/orders/formatOrderData");
 
 const createProductHandler = async (req, res) => {
   console.log(`POST ${req.originalUrl}`);
 
   const data = req.body;
 
-    try{
-      productDataValidation.validateAllData(data);
+  try {
+    productDataValidation.validateAllData(data);
 
-      const newProduct = await productServices.create(data);
+    const newProduct = await productServices.create(data);
 
-      return res.status(201).json(newProduct);
-
-    }catch(err){
-      if(err.status) return res.status(err.status).json({message: err.message})
-    }
+    return res.status(201).json(newProduct);
+  } catch (err) {
+    if (err.status)
+      return res.status(err.status).json({ message: err.message });
+  }
 };
 
 const editProductHandler = async (req, res) => {
@@ -27,80 +27,79 @@ const editProductHandler = async (req, res) => {
 
   const data = req.body;
 
-  const {productId} = req.params;
+  const { productId } = req.params;
 
   try {
     productDataValidation.validateAllData(data);
 
     await productServices.findById(productId);
 
-    await productServices.findByIdAndUpdate(productId,data);
+    await productServices.findByIdAndUpdate(productId, data);
 
-    return res.status(200).json({message:`You successfully updated ${productId}!`});
-
+    return res
+      .status(200)
+      .json({ message: `You successfully updated ${productId}!` });
   } catch (err) {
-    if(err.path === "_id") res.status(400).json({message:"ProductId is not in valid format"});
-    if(err.status) return res.status(err.status).json({ message:err.message});
+    if (err.path === "_id")
+      res.status(400).json({ message: "ProductId is not in valid format" });
+    if (err.status)
+      return res.status(err.status).json({ message: err.message });
   }
 };
 
-
-const deleteProductHandler = async(req,res) => {
+const deleteProductHandler = async (req, res) => {
   console.log(`DELETE ${req.originalUrl}`);
 
-  const {productId} = req.params;
+  const { productId } = req.params;
 
   try {
     const product = await productServices.findById(productId);
     product.inStock = false;
     for (const size in product.sizes) {
-      product.sizes[size] = 0
-      
+      product.sizes[size] = 0;
     }
-    await productServices.findByIdAndUpdate(product._id,product);
+    await productServices.findByIdAndUpdate(product._id, product);
 
-    res.status(200).json({message:"You successfully deleteted this product!"});
-
-  }catch(err){
-    if(err.path === "_id") res.status(400).json({message:"ProductId is not in valid format"});
-    if(err.status) return res.status(err.status).json({message:err.message});
+    res
+      .status(200)
+      .json({ message: "You successfully deleteted this product!" });
+  } catch (err) {
+    if (err.path === "_id")
+      res.status(400).json({ message: "ProductId is not in valid format" });
+    if (err.status)
+      return res.status(err.status).json({ message: err.message });
     console.log(err);
   }
-}
+};
 
-
-const editOrderHandler = async (req,res) => {
+const editOrderHandler = async (req, res) => {
   console.log(`EDIT ${req.originalUrl}`);
 
-  const {orderId} = req.params;
-  const {status} = req.body;
+  const { orderId } = req.params;
+  const { status } = req.body;
 
   try {
     const order = await orderServices.findById(orderId);
-
   } catch (error) {
-    if(err.path === "_id") res.status(400).json({message:"ProductId is not in valid format"});
-    if(err.status) return res.status(err.status).json({message:err.message});
+    if (err.path === "_id")
+      res.status(400).json({ message: "ProductId is not in valid format" });
+    if (err.status)
+      return res.status(err.status).json({ message: err.message });
   }
+};
 
-}
-
-const getAllOrdersHandler = async(req,res) => {
+const getAllOrdersHandler = async (req, res) => {
   console.log(`GET ${req.originalUrl}`);
-  const  orders = await orderServices.getAll();
-  for (const order of orders) {
-    if(order.profileId) {
-      order.profileId.password = null
-    }
-  }
-  res.status(200).json([...orders])
+  const orders = await orderServices.getAll();
+  const ordersInformation = formatOrderData(orders);
+  res.status(200).json([...ordersInformation]);
   res.end();
-}
+};
 
-const checkAccessToken = async (req,res) => {
-    console.log(`POST ${req.originalUrl}`);
-    return res.status(200).json({isAdmin:true});
-}
+const checkAccessToken = async (req, res) => {
+  console.log(`POST ${req.originalUrl}`);
+  return res.status(200).json({ isAdmin: true });
+};
 
 router.post("/checkToken", checkAccessToken);
 
@@ -110,7 +109,5 @@ router.delete("/products/:productId/delete", deleteProductHandler);
 
 router.post("/orders", getAllOrdersHandler);
 router.put("/orders/:orderId", editOrderHandler);
-
-
 
 module.exports = router;
