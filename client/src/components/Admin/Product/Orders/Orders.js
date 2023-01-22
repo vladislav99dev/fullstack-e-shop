1024e0ed30ae7e0df2aef9255be3e8615eefa7e3
vladislav99dev <vladislavcho99@abv.bg>
2023-01-22 17:58:42 +0200

    add spinner and make request to api when upodating an norder

--- client/src/components/Admin/Product/Orders/Orders.js
+++ client/src/components/Admin/Product/Orders/Orders.js
@@ -1,18 +1,19 @@
-import React, { useState, useEffect,useReducer } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import ordersRequester from "../../../../services/ordersRequester";
 import { useAuthContext } from "../../../../context/AuthContext";
 import isAdmin from "../../../../HOC/adminRoutesGuard";
 import modalMessages from "../../../../HOC/modalMessages";
 import { useModalsContext } from "../../../../context/ModalsContext";
 import Order from "./Order";
-import styles from "./Orders.module.css"
-
+import Spinner from "../../../Spinner/Spinner";
+import styles from "./Orders.module.css";
 
 const Orders = () => {
-  const [orders, setOrders] = useState();
+  const [orders, setOrders] = useState([]);
   const { user } = useAuthContext();
   const { setFailedModal } = useModalsContext();
-  const [currImage,setCurrImage] =useState();
+  const [isLoading,setIsLoading] = useState(true);
+  // const [currImage, setCurrImage] = useState();
 
   useEffect(() => {
     ordersRequester
@@ -20,20 +21,50 @@ const Orders = () => {
       .then((response) => {
         if (response.status === 200) return response.json();
       })
-      .then((response) => setOrders(response) );
+      .then((response) => {
+        setOrders(response)
+        setIsLoading(false)
+      });
   }, []);
 
+  const updateOrderHandler = async (orderId,orderStatus) => {
+    setIsLoading(true)
+    try{
+      const response = await ordersRequester.updateOrder({orderStatus,profileId:user._id},user.accessToken,orderId);
+      const jsonResponse = await response.json();
+      if(response.status === 200)  {
+        console.log(orders);
+        setOrders(jsonResponse.formatedOrders);
+        console.log(orders);
+        setTimeout(()=>{
+          setIsLoading(false);
+        },2000)
+        
+      };
+    }catch(err){
+      console.log(err);
+    }
+  };
 
-
-  return <div className={styles.container}>
-    <div className={`${styles['orders-container']}`}>
-    {orders?.map((order) => {
-      return (
-        <Order key={order.orderInfo.orderId} data={order} />
-        )
-      })}
-      </div>
-  </div>;
+  return (
+    <>
+    {isLoading ? <Spinner/> : (
+      <div className={styles.container}>
+      <div className={`${styles["orders-container"]}`}>
+      {orders?.map((order) => {
+        return (
+          <Order
+          key={order.orderInfo.orderId}
+          data={order}
+          updateOrderHandler={updateOrderHandler}
+            />
+            );
+          })}
+          </div>
+          </div>
+           )} 
+          </>
+  );
 };
 
 export default isAdmin(modalMessages(Orders));
